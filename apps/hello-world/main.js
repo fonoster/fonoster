7cdd4be22f932a7e86ab0a93764324c3d92a5b1a
Fonoster Team <fonosterteam@fonoster.com>
2019-02-18 16:53:55 -0500

    Refactored

--- apps/hello-world/main.js
+++ apps/hello-world/main.js
@@ -1,10 +1,10 @@
-var AGIServer = require('ding-dong');
+const AGIServer = require('ding-dong');
 
-var handler = function (context) {
-  //context.answer()
-  context.streamFile('beep')
-  //context.hangup()
-};
+function main(channel) {
+  channel.answer()
+  channel.streamFile('beep')
+  channel.hangup()
+}
 
-var agi = new AGIServer(handler, {debug: true});
+var agi = new AGIServer(main, {debug: true});
 agi.start(4573);
