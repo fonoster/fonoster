f345927d6d977e1bebd3b7d2180bfdaf512cb015
Fonoster Team <fonosterteam@fonoster.com>
2019-02-18 16:47:53 -0500

    Replaced ARI with AGI for media controlling

--- apps/hello-world/main.js
+++ apps/hello-world/main.js
@@ -1,8 +1,10 @@
-const functions = require('./yaps-functions')
+var AGIServer = require('ding-dong');
 
-functions.config(func)
+var handler = function (context) {
+  //context.answer()
+  context.streamFile('beep')
+  //context.hangup()
+};
 
-function func(channel) {
-  channel.answer()
-  channel.play({media: 'sound:hello-world'})
-}
+var agi = new AGIServer(handler, {debug: true});
+agi.start(4573);
