da49fcdf16ba225c277e9cacc1147d9ba4a1b416
Fonoster Team <fonosterteam@fonoster.com>
2019-02-18 22:32:58 -0500

    Improved containers booting synchronization

--- apps/hello-world/main.js
+++ apps/hello-world/main.js
@@ -1,9 +1,17 @@
 const AGIServer = require('ding-dong');
 
-function main(channel) {
-  channel.answer()
-  channel.streamFile('beep')
-  channel.hangup()
+function main(context) {
+  context.onEvent('variables')
+      .then(function (vars) {
+          console.log('vars', vars);
+          return context.streamFile('beep');
+      })
+      .then(function (result) {
+          return context.setVariable('RECOGNITION_RESULT', 'I\'m your father, Luc');
+      })
+      .then(function (result) {
+          return context.end();
+      })
 }
 
 var agi = new AGIServer(main, {debug: true});
