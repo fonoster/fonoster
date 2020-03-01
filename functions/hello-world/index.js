621f13cff1407dbde230d017987c484188c9d217
Pedro Sanders <sanderspedro@gmail.com>
2020-03-01 15:54:02 -0500

    Updated example

--- functions/hello-world/index.js
+++ functions/hello-world/index.js
@@ -3,7 +3,17 @@
  * @since v1
  */
 module.exports = chan => {
+    // Wait for three seconds
     chan.wait(3)
-    const result = chan.gather(chan.say('beep'), { maxDigits: 5 })
+
+    // Play audio from url
+    chan.play('https://www.nch.com.au/acm/8k16bitpcm.wav')
+
+    // Play audio from text (using the default tts)
+    // The second time it will run faster because it is cache
+    chan.say('Hello world!')
+
+    // This verb waits for the user to input from the keypad
+    const result = chan.gather(chan.play('beep'), { maxDigits: 5 })
     console.log(`result: ${result}`)
 }
