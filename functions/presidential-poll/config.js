3eabf7cd597b13eaaad0ecc0b5e4821321a21ae6
Pedro Sanders <sanderspedro@gmail.com>
2020-03-03 22:29:27 -0500

    Added example

---
+++ functions/presidential-poll/config.js
@@ -0,0 +1,19 @@
+const yaps = require('@yaps/core/app')
+
+const prodConfig = {
+    projectId: "tts-storage",
+    apiKey: "AIzaSyAshRmJgF-p3IesZfmeLzhB-n705ePUAFA",
+    storageBucket: "default"
+}
+
+const devConfig = {
+    projectId: "tts-storage",
+    apiKey: "AIzaSyAshRmJgF-p3IesZfmeLzhB-n705ePUAFA",
+    storageBucket: "default-test"
+}
+
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig
+
+yaps.initializeApp(config)
+module.exports.storage = yaps.storage()
+module.exports.logger = yaps.logging()
---
+++ functions/presidential-poll/index.js
@@ -0,0 +1,71 @@
+/**
+ * @author Pedro Sanders
+ * @since v1
+ */
+const { storage, logger } = require('./config')
+
+module.exports = chan => {
+
+    logger.log('Handler got call %s -> %s',
+        chan.channel.request.callerid, chan.channel.request.extension)
+
+    const candidates = {
+      '1': { name: 'Donald Trump' },
+      '2': { name: 'Hillary Clinton' },
+      '3': {name: 'Beny Sanders' }
+    }
+
+    // Play audio from url
+    //chan.play('https://www.nch.com.au/acm/8k16bitpcm.wav')
+
+    // This will run slow the first time.
+    chan.say('Hello, this is a presidential poll from the Georgia Statistics Center.')
+
+    const key = chan.gather(chan.say('If you would like to be remove from our list, press 7'),
+      {timeout: 2, numDigits: 1})
+
+    logger.log(`Remove from list? ${key}`)
+
+    if (key == 7) {
+        // This address does not exist, it its only for ilustration
+        http.post('https://georgia.gov/removeFromList')
+          .timeout(1200)
+          .basicAuth('username', 'password')
+          .then( result => {
+              if (result.body === 'OK') {
+                  chan.say('You have been remove from our list')
+              } else {
+                // Do nothing for now
+              }
+        })
+    }
+
+    const config = {
+        finishOnKey: '*',
+        timeout: 1,
+        numDigits: 1
+    }
+
+    while(true) {
+        const key = chan.gather(
+          chan.say('For Trump Press one. For Hillary, press two and for Sanders 3'),
+            config)
+
+        if (['1', '2', '3'].includes(key)) {
+            chan.say('I could not register your selection. Lets try again.')
+            // You can use Play to stream a pre-recorded audio
+            chan.play('beep')
+            continue
+        }
+
+        chan.say(`You selected option ${key}, ${candidates[key].name}`)
+        // Store the result as part of the cdr
+        chan.stash('candidate', key)
+
+        break
+    }
+
+    chan.say('Thank you for participating. Review the results at www.georgia.gov. Goodbye!')
+
+    logger.log(`Ended call-id => ${chan.request.id}, time => ${Date.now()}`)
+}
