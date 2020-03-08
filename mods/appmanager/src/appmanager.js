668750e225cce4e63c99d5a1c2cebefccbb7bc29
Pedro Sanders <sanderspedro@gmail.com>
2020-03-08 16:13:19 -0400

    Improved client API

--- mods/appmanager/src/appmanager.js
+++ mods/appmanager/src/appmanager.js
@@ -2,15 +2,35 @@
  * @author Pedro Sanders
  * @since v1
  */
-const { appManagerService } = require('@yaps/core').client
+const { AbstractService } = require('@yaps/core')
+const { AppManager, grpc } = require('@yaps/core').client
+// const grpc = require('grpc') Using this causes issues
+// for now I'm just hacking this by exporting/import grpc
 const promisifyAll = require('grpc-promise').promisifyAll
+const {
+    getClientCredentials
+} = require('@yaps/core').trust_util
 
-module.exports = function() {
-    promisifyAll(appManagerService)
-    this.listApps = async () => appManagerService.listApps().sendMessage()
-    this.getApp = async (ref) => appManagerService.getApp().sendMessage({ref})
-    this.createApp = async (request) => appManagerService.createApp().sendMessage(request)
-    this.updateApp = async (request) => appManagerService.updateApp().sendMessage(request)
-    this.deleteApp = async (ref) => appManagerService.deleteApp().sendMessage({ref})
-    return this
+class AppManagerSrv extends AbstractService {
+
+    constructor(options) {
+        super(options)
+
+        const metadata = new grpc.Metadata()
+        metadata.add('access_key_id', super.getOptions().accessKeyId)
+        metadata.add('access_key_secret', super.getOptions().accessKeySecret)
+
+        const client = new AppManager(super.getOptions().endpoint,
+          getClientCredentials())
+
+        promisifyAll(client, {metadata})
+
+        this.listApps = () => client.listApps().sendMessage()
+        this.getApp = ref => client.getApp().sendMessage({ref})
+        this.createApp = request => client.createApp().sendMessage(request)
+        this.updateApp = request => client.updateApp().sendMessage(request)
+        this.deleteApp = ref => client.deleteApp().sendMessage({ref})
+    }
 }
+
+module.exports = AppManagerSrv
