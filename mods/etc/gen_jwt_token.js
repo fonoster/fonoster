3a3c895680fe050b948a8259f24dc1fc83cf2e94
Pedro Sanders <sanderspedro@gmail.com>
2020-03-11 17:39:23 -0400

    Improved token generator

--- mods/etc/gen_jwt_token.js
+++ mods/etc/gen_jwt_token.js
@@ -1,12 +1,18 @@
-const jwt = require('njwt')
+const jwt = require('jsonwebtoken')
 const fs = require('fs')
 const path = require('path')
 
-// Getting SALT
-const pathToSalt = path.join(process.env.CERTS_PATH, 'jwt.salt')
-const result = fs.readFileSync(pathToSalt).toString().trim()
+const pathToCerts = process.env.CERTS_PATH || path.join(__dirname, 'certs')
+const accessKeyId = process.env.ACCESS_KEY_ID || 'yaps'
 
-const claims = { iss: 'yaps', sub: process.env.ACCESS_KEY_ID }
-const token = jwt.create(claims, result)
-// token.setExpiration(new Date().getTime() + 3600*1000)
-console.log(`${process.env.ACCESS_KEY_ID}:${token.compact()}`)
+// Getting SALT
+const pathToSalt = path.join(pathToCerts, 'jwt.salt')
+const privateKey = fs.readFileSync(pathToSalt).toString().trim()
+
+const claims = {
+  iss: 'yaps',
+  sub: accessKeyId,
+  exp: Math.floor(Date.now('2030-01-01') / 1000)
+}
+
+console.log(`${accessKeyId}:${jwt.sign(claims, privateKey)}`)
