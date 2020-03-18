da10e2ce2d63b5363c3ce004b413f523ac1f3e83
Pedro Sanders <sanderspedro@gmail.com>
2020-03-18 11:24:07 -0400

    Added api

--- mods/sdk/src/sdk.js
+++ mods/sdk/src/sdk.js
@@ -4,6 +4,8 @@
  */
 const { AppManager } = require('@yaps/appmanager')
 const { Storage } = require('@yaps/storage')
+const { Numbers } = require('@yaps/numbers')
 
 module.exports.AppManager = AppManager
 module.exports.Storage = Storage
+module.exports.Numbers = Numbers
