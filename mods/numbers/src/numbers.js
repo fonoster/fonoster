b8dbfc7753a6dcb81b8d2f28dfe9704c6fa87f79
Pedro Sanders <sanderspedro@gmail.com>
2020-03-23 06:58:09 -0400

    Fixed numbers class

--- mods/numbers/src/numbers.js
+++ mods/numbers/src/numbers.js
@@ -6,80 +6,67 @@ const { getClientCredentials } = require('@yaps/core').trust_util
 
 class Numbers extends AbstractService {
   constructor (options) {
-    super(options)
+    super(options, NumbersService.NumbersClient)
+    promisifyAll(super.getService(), { metadata: super.getMeta() })
+  }
 
-    const metadata = new grpc.Metadata()
-    metadata.add('access_key_id', super.getOptions().accessKeyId)
-    metadata.add('access_key_secret', super.getOptions().accessKeySecret)
-
-    const credentials = grpc.credentials.createInsecure()
-
+  async createNumber (request) {
     logger.log(
-      'info',
-      `Connecting with API Server @ ${super.getOptions().endpoint}`
+      'verbose',
+      `@yaps/numbers createNumber [request: ${JSON.stringify(request)}]`
+    )
+    logger.log(
+      'debug',
+      `@yaps/numbers createNumber [validating number:  ${request.e164Number}]`
     )
 
-    const service = new NumbersService.NumbersClient(
-      super.getOptions().endpoint,
-      credentials
+    // TODO: Validate number
+
+    const number = new NumbersPB.Number()
+    number.setE164Number(request.e164Number)
+    number.setIngressApp(request.ingressApp)
+
+    const req = new NumbersPB.CreateNumberRequest()
+    req.setNumber(number)
+
+    return super
+      .getService()
+      .createNumber()
+      .sendMessage(req)
+  }
+
+  async getIngressApp (request) {
+    logger.log(
+      'verbose',
+      `@yaps/numbers getIngressApp [request: ${JSON.stringify(request)}]`
+    )
+    logger.log(
+      'debug',
+      `@yaps/numbers getIngressApp [validating number:  ${request.e164Number}]`
     )
 
-    promisifyAll(service, { metadata })
+    const req = new NumbersPB.GetIngressAppRequest()
+    req.setE164Number(request.e164Number)
 
-    this.createNumber = request => {
-      logger.log(
-        'verbose',
-        `@yaps/numbers createNumber [request: ${JSON.stringify(request)}]`
-      )
-      logger.log(
-        'debug',
-        `@yaps/numbers createNumber [validating number:  ${request.e164Number}]`
-      )
+    return super
+      .getService()
+      .getIngressApp()
+      .sendMessage(req)
+  }
 
-      // TODO: Validate number
+  async getIngressAppSync (request) {
+    const sleep = require('sync').sleep
+    let result
+    let error
+    this.getIngressApp(request)
+      .then(r => (result = r))
+      .catch(e => (error = e))
 
-      const number = new NumbersPB.Number()
-      number.setE164Number(request.e164Number)
-      number.setIngressApp(request.ingressApp)
+    while (result === undefined && error === undefined) sleep(100)
 
-      const req = new NumbersPB.CreateNumberRequest()
-      req.setNumber(number)
+    if (error) throw error
 
-      return service.createNumber().sendMessage(req)
-    }
-
-    this.getIngressApp = request => {
-      logger.log(
-        'verbose',
-        `@yaps/numbers getIngressApp [request: ${JSON.stringify(request)}]`
-      )
-      logger.log(
-        'debug',
-        `@yaps/numbers getIngressApp [validating number:  ${
-          request.e164Number
-        }]`
-      )
-
-      const req = new NumbersPB.GetIngressAppRequest()
-      req.setE164Number(request.e164Number)
-
-      return service.getIngressApp().sendMessage(req)
-    }
-
-    this.getIngressAppSync = request => {
-      const sleep = require('sync').sleep
-      let result
-      let error
-      this.getIngressApp(request)
-        .then(r => (result = r))
-        .catch(e => (error = e))
-
-      while (result === undefined && error === undefined) sleep(100)
-
-      if (error) throw error
-
-      return result
-    }
+    return result
   }
 }
 
