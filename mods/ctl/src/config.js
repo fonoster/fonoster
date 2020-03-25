f9b3fa3ded6d27bc3065c8394b0ce4e55bc0553d
Pedro Sanders <sanderspedro@gmail.com>
2020-03-24 23:14:24 -0400

    Removed env loading

--- mods/ctl/src/config.js
+++ mods/ctl/src/config.js
@@ -1,8 +1,2 @@
-if (process.env.NODE_ENV === 'dev') {
-  require('dotenv').config({ path: __dirname + '/../../.env' })
-} else {
-  console.log('running production env')
-}
-
 const { logger } = require('@yaps/core')
 logger.transports.forEach(t => (t.silent = true))
