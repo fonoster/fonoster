e3ca7509b5a9531d47c599a99320e897b606b77c
Pedro Sanders <sanderspedro@gmail.com>
2020-03-25 00:09:13 -0400

    Added dependency

--- mods/sdk/src/sdk.js
+++ mods/sdk/src/sdk.js
@@ -1,7 +1,9 @@
 const { AppManager } = require('@yaps/appmanager')
 const { Storage } = require('@yaps/storage')
 const { Numbers } = require('@yaps/numbers')
+const { Domains } = require('@yaps/domains')
 
 module.exports.AppManager = AppManager
 module.exports.Storage = Storage
 module.exports.Numbers = Numbers
+module.exports.Domains = Domains
