4641bbe4db6885ffb58373205915e8a93a901f14
Pedro Sanders <sanderspedro@gmail.com>
2020-03-27 17:28:58 -0400

    Cleanup

--- mods/core/src/common/trust_util.js
+++ mods/core/src/common/trust_util.js
@@ -16,24 +16,36 @@ const SERVER_KEY = process.env.CERTS_PATH + '/server.key'
 const CLIENT_CRT = process.env.CERTS_PATH + '/client.crt'
 const CLIENT_KEY = process.env.CERTS_PATH + '/client.key'
 
-module.exports.getServerCredentials = () =>
-  grpc.ServerCredentials.createSsl(
-    fs.readFileSync(CA_CRT),
-    [
-      {
-        cert_chain: fs.readFileSync(SERVER_CRT),
-        private_key: fs.readFileSync(SERVER_KEY)
-      }
-    ],
-    true
-  )
+module.exports.getServerCredentials = () => {
+  try {
+    return grpc.ServerCredentials.createSsl(
+      fs.readFileSync(CA_CRT),
+      [
+        {
+          cert_chain: fs.readFileSync(SERVER_CRT),
+          private_key: fs.readFileSync(SERVER_KEY)
+        }
+      ],
+      true
+    )
+  } catch (e) {
+    logger.log('error', 'Unable to load certificates')
+    process.exit(1)
+  }
+}
 
-module.exports.getClientCredentials = () =>
-  grpc.credentials.createSsl(
-    fs.readFileSync(CA_CRT),
-    fs.readFileSync(CLIENT_KEY),
-    fs.readFileSync(CLIENT_CRT)
-  )
+module.exports.getClientCredentials = () => {
+  try {
+    return grpc.credentials.createSsl(
+      fs.readFileSync(CA_CRT),
+      fs.readFileSync(CLIENT_KEY),
+      fs.readFileSync(CLIENT_CRT)
+    )
+  } catch (e) {
+    logger.log('error', 'Unable to load certificates')
+    process.exit(1)
+  }
+}
 
 module.exports.auth = function (call, callback) {
   let salt
