1dfc36bf318528a238de75f71f7e304f3b7be645
Pedro Sanders <sanderspedro@gmail.com>
2020-03-28 00:29:07 -0400

    Updated generator

--- mods/etc/gen_certs.sh
+++ mods/etc/gen_certs.sh
@@ -20,7 +20,7 @@ openssl req -passin pass:1111 -new -x509 -days 365 -key ca.key -out ca.crt -subj
 
 openssl genrsa -passout pass:1111 -des3 -out server.key 4096
 
-openssl req -passin pass:1111 -new -key server.key -out server.csr -subj  "/C=US/ST=WA/L=Seattle/O=Test/OU=Server/CN=localhost"
+openssl req -passin pass:1111 -new -key server.key -out server.csr -subj  "/C=US/ST=WA/L=Seattle/O=Test/OU=Server/CN=apiserver"
 
 openssl x509 -req -passin pass:1111 -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
 
@@ -28,7 +28,7 @@ openssl rsa -passin pass:1111 -in server.key -out server.key
 
 openssl genrsa -passout pass:1111 -des3 -out client.key 4096
 
-openssl req -passin pass:1111 -new -key client.key -out client.csr -subj  "/C=US/ST=WA/L=Seattle/O=Test/OU=Client/CN=localhost"
+openssl req -passin pass:1111 -new -key client.key -out client.csr -subj  "/C=US/ST=WA/L=Seattle/O=Test/OU=Client/CN=integrationtests"
 
 openssl x509 -passin pass:1111 -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out client.crt
 
