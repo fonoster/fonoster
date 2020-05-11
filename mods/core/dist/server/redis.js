f875b4801cabc2a25f334657f7c07c62579b6bc9
Pedro Sanders <sanderspedro@gmail.com>
2020-05-11 18:55:16 -0400

    Deleted

--- mods/core/dist/server/redis.js
+++
@@ -1,5 +0,0 @@
-const Redis = require('ioredis')
-const host = process.env.APISERVER_DS_HOST || 'localhost'
-const port = process.env.APISERVER_DS_PORT || 6379
-module.exports = new Redis(`redis://${host}:${port}`)
-//# sourceMappingURL=redis.js.map
