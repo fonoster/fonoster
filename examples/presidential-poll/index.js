8a3998d857901b26f346fa1b9f50800a8cc220f7
Pedro Sanders <sanderspedro@gmail.com>
2020-05-25 21:16:07 -0400

    Cleanup

--- examples/presidential-poll/index.js
+++ examples/presidential-poll/index.js
@@ -1,21 +1,24 @@
+
+const config = {
+    finishOnKey: '*',
+    timeout: 1,
+    numDigits: 1
+}
+
+const candidates = {
+    '1': { name: 'Donald Trump' },
+    '2': { name: 'Hillary Clinton' },
+    '3': {name: 'Beny Sanders' }
+}
+
 /**
  * This example shows the using of most all Fonos verbs.
  */
 module.exports = chan => {
-
     console.log('Handler got call %s -> %s',
         chan.callDetailRecord.ref, chan.callDetailRecord.to)
 
-    const candidates = {
-      '1': { name: 'Donald Trump' },
-      '2': { name: 'Hillary Clinton' },
-      '3': {name: 'Beny Sanders' }
-    }
-
-    // Play audio from url
-    //chan.play('https://www.nch.com.au/acm/8k16bitpcm.wav')
-
-    // This will run slow the first time.
+    // This will run faster the second time because file is place in a cache
     chan.say('Hello, this is a presidential poll from the Georgia Statistics Center.')
 
     const key = chan.gather(chan.say('If you would like to be remove from our list, press 7'),
@@ -23,26 +26,29 @@ module.exports = chan => {
 
     console.log(`Remove from list? ${key}`)
 
-    if (key == 7) {
-        // This address does not exist, it its only for ilustration
-        http.post('https://georgia.gov/removeFromList')
-          .timeout(1200)
-          .basicAuth('username', 'password')
-          .then( result => {
-              if (result.body === 'OK') {
-                  chan.say('You have been remove from our list')
-              } else {
-                // Do nothing for now
-              }
-        })
-    }
+    if (key === 7) removeFromList()
 
-    const config = {
-        finishOnKey: '*',
-        timeout: 1,
-        numDigits: 1
-    }
+    runMenu()
 
+    chan.say('Thank you for participating. Review the results at www.georgia.gov. Goodbye!')
+    console.log(`Call ${chan.callDetailRecord.ref} ${Date.now()} ended`)
+}
+
+function removeFromList(chan) {
+    // This address does not exist, it its only for ilustration
+    http.post('https://georgia.gov/removeFromList')
+    .timeout(1200)
+    .basicAuth('username', 'password')
+    .then( result => {
+        if (result.body === 'OK') {
+            chan.say('You have been remove from our list')
+        } else {
+        // Do nothing for now
+        }
+    })
+}
+
+function runMenu (chan) {
     while(true) {
         const key = chan.gather(
           chan.say('For Trump Press one. For Hillary, press two and for Sanders 3'),
@@ -61,8 +67,4 @@ module.exports = chan => {
 
         break
     }
-
-    chan.say('Thank you for participating. Review the results at www.georgia.gov. Goodbye!')
-
-    console.log(`Call ${chan.callDetailRecord.ref} ${Date.now()} ended`)
 }
