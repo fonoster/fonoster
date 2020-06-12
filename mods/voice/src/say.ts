c1ceb17727478a82f758250df97a8a36c67aaaa1
Pedro Sanders <sanderspedro@gmail.com>
2020-06-11 21:21:41 -0400

    Refactored

--- mods/voice/src/say.ts
+++ mods/voice/src/say.ts
@@ -7,50 +7,49 @@ import { transcodeSync, computeFilename } from '@fonos/tts'
 class Say extends Verb {
   constructor (channel: any, config: any) {
     super(channel, config)
+    if (!config.tts) throw 'Not tts engine found'
+    if (!config.storage) throw 'Not storage object found'
+    if (!config.bucket) throw 'Not bucket found'
+  }
+
+  private synth (text: string, filename: string, options?: any): string {
+    const pathToFile = this.config.tts.synthesizeSync(text, options)
+
+    const pathToTranscodedFile = path.join(path.dirname(pathToFile), filename)
+    transcodeSync(pathToFile, pathToTranscodedFile)
+
+    const metadata = { 'Content-Type': 'audio/x-wav' }
+    this.config.storage.uploadObjectSync({
+      filename: pathToTranscodedFile,
+      bucket: this.config.bucket,
+      metadata
+    })
+
+    return this.config.storage.getObjectURLSync({
+      name: filename,
+      bucket: this.config.bucket
+    })
   }
 
   run (text: string, options: any): string {
     if (!text) throw new Error('You must provide a text.')
-    if (!this.config.tts) throw new Error('Not tts engine found')
-    if (!this.config.storage) throw new Error('Not storage object found')
-    if (!this.config.bucket) throw new Error('Not bucket found')
-
-    // The final format pushed to the bucket will always be .wav
-    const metadata = { 'Content-Type': 'audio/x-wav' }
     const filename = 't_' + computeFilename(text, options)
 
-    let url
-
     try {
-      url = this.config.storage.getObjectURLSync({
+      let url = this.config.storage.getObjectURLSync({
         name: filename,
         bucket: this.config.bucket
       })
+
+      if (!url) url = this.synth(text, filename, options)
+
+      return new Play(this.config, this.channel).run(url, options)
     } catch (e) {
       logger.log(
         'silly',
         `@fonos/vouice.YapsWrapperChannel.say [no url found for file ${filename}]`
       )
     }
-
-    if (url === undefined) {
-      const pathToFile = this.config.tts.synthesizeSync(text, options)
-      const pathToTranscodedFile = path.join(path.dirname(pathToFile), filename)
-      transcodeSync(pathToFile, pathToTranscodedFile)
-
-      this.config.storage.uploadObjectSync({
-        filename: pathToTranscodedFile,
-        bucket: this.config.bucket,
-        metadata
-      })
-
-      url = this.config.storage.getObjectURLSync({
-        name: filename,
-        bucket: this.config.bucket
-      })
-    }
-
-    return new Play(this.config, this.channel).run(url, options)
   }
 }
 
