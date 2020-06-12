f54ca7b5fc1c75b91573cfadd73f49e5f884b66d
Pedro Sanders <sanderspedro@gmail.com>
2020-06-11 21:36:24 -0400

    Refactored

--- mods/voice/src/play.ts
+++ mods/voice/src/play.ts
@@ -1,32 +1,33 @@
 import Verb from './verb'
 
+interface PlayOptions {
+  finishOnKey: string
+}
+
+const validate = (file: string, options: PlayOptions) => {
+  const { finishOnKey = '#' } = options
+  if (!file) throw 'you must indicate a file.'
+  if (
+    finishOnKey &&
+    (finishOnKey.length !== 1 || '1234567890#*'.indexOf(finishOnKey) < 0)
+  )
+    throw `Invalid finishOnKey parameter: found ${finishOnKey} but must be a single digit type of 0-9,#,*`
+}
+
 class Play extends Verb {
   constructor (config: any, channel: any) {
     super(config, channel)
   }
 
-  run (file: string, options?: any) {
-    if (!file) throw new Error('you must indicate a file.')
-    let finishOnKey = '#'
-
-    if (options) {
-      if (
-        options.finishOnKey &&
-        (options.finishOnKey.length !== 1 ||
-          '1234567890#*'.indexOf(options.finishOnKey) < 0)
-      )
-        throw new Error(
-          `Invalid finishOnKey parameter: found ${options.finishOnKey} but must be a single digit type of 0-9,#,*`
-        )
-
-      if (options.finishOnKey) finishOnKey = options.finishOnKey
-    }
+  run (file: string, options?: PlayOptions) {
+    const { finishOnKey = '#' } = options
+    validate(file, options)
 
     const result = this.channel.streamFile(file, finishOnKey)
 
     if (result.code === 200) return result.attributes.result
 
-    throw new Error(result.rawReply)
+    throw result.rawReply
   }
 }
 
