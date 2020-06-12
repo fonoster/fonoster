52264efce3956aeb9ab95670e219d9ff599e9bdc
Pedro Sanders <sanderspedro@gmail.com>
2020-06-12 15:51:25 -0400

    Refactored

--- mods/storage/src/storage.ts
+++ mods/storage/src/storage.ts
@@ -1,13 +1,55 @@
 import logger from '@fonos/logger'
 import { FonosService, StorageService, StoragePB } from '@fonos/core'
 
+/**
+ * @classdesc Use Fonos Storage, a capability of Fonos Object Storage subsystem,
+ * to upload, download, and delete objects.
+ *
+ * @extends FonosService
+ * @example
+ *
+ * const Fonos = require('@fonos/sdk')
+ * const storage = new Fonos.Storage()
+ *
+ * storage.uploadObject()
+ * .then(result => {
+ *    console.log(result)            // successful response
+ * }).catch(e => console.error(e))   // an error occurred
+ */
 class Storage extends FonosService {
+  /**
+   * Constructs a new Storage object.
+   *
+   * @see module:core:FonosService
+   */
   constructor (options: any) {
     super(StorageService.StorageClient, options)
     super.init()
   }
 
-  async getObjectURL (request: { name: string; bucket: string }) {
+  /**
+   * Get Object URL.
+   *
+   * @param {Object} request - Object with information about the location and
+   * and name of the requested object
+   * @param {string} request.name - The name of the object
+   * @param {string} request.bucket - Bucket where object is located
+   * save your file.
+   * @return {Promise<string>} localy accessible URL to the object
+   * @throws if bucket or object does not exist
+   * @example
+   *
+   * const request = {
+   *    name: 'object-name',
+   *    bucket: 'bucket-name'
+   * }
+   *
+   * storage.getObjectURL(request)
+   * .then(result => {
+   *   console.log(result)
+   * }).catch(e => console.error(e))  // an error occurred
+   */
+  async getObjectURL (request: { name: string; bucket: string }): Promise<any> {
     return new Promise((resolve, reject) => {
       logger.log(
         'verbose',
