f86994ab747793ddb085ad34c46460c23a73b66a
Pedro Sanders <sanderspedro@gmail.com>
2020-06-13 14:50:42 -0400

    Fixed auth

--- mods/core/src/server/server.ts
+++ mods/core/src/server/server.ts
@@ -5,6 +5,9 @@ import StorageServer, {
   IStorageServer,
   StorageService
 } from './storage/storage'
+import AppManagerServer, { AppManagerService } from './appmanager/appmanager'
+import { IAppManagerServer } from './protos/appmanager_grpc_pb'
+import createAccessFile, { accessExist } from '@fonos/certs'
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'dev') {
   const env = path.join(__dirname, '..', '..', '..', '..', '.env')
@@ -12,14 +15,18 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'dev') {
 }
 
 async function main () {
-  //if (!accessExist()) {
-  //  logger.log('info', `No access file found. Creating access file`)
-  //  await createAccessFile()
-  //}
+  if (!accessExist()) {
+    logger.log('info', `No access file found. Creating access file`)
+    await createAccessFile()
+  }
 
   const server = new grpc.Server()
   const endpoint = process.env.BINDADDR || '0.0.0.0:50052'
   server.addService<IStorageServer>(StorageService, new StorageServer())
+  server.addService<IAppManagerServer>(
+    AppManagerService,
+    new AppManagerServer()
+  )
   server.bind(endpoint, grpc.ServerCredentials.createInsecure())
   server.start()
 
