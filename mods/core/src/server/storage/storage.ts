db26ed49d0b6988c1e1ee31ea1990b255e696f80
Pedro Sanders <sanderspedro@gmail.com>
2020-06-13 14:50:06 -0400

    Fixed auth

--- mods/core/src/server/storage/storage.ts
+++ mods/core/src/server/storage/storage.ts
@@ -1,26 +1,25 @@
 import grpc from 'grpc'
+import getObjectURL from './get_object_url'
+import uploadObject from './upload_object'
+import { FonosAuthError } from '@fonos/errors'
 import {
   UploadObjectRequest,
   UploadObjectResponse,
   GetObjectURLRequest,
   GetObjectURLResponse
 } from '../protos/storage_pb'
+import { auth } from '../../common/trust_util'
 import { IStorageServer, StorageService } from '../protos/storage_grpc_pb'
-import getObjectURL from './get_object_url'
-import uploadObject from './upload_object'
 
 class StorageServer implements IStorageServer {
   uploadObject (
     call: grpc.ServerReadableStream<UploadObjectRequest>,
     callback: grpc.sendUnaryData<UploadObjectResponse>
   ): void {
-    //if (!auth(call)) return callback(new FonosAuthError())
+    if (!auth(call)) return callback(new FonosAuthError(), null)
 
     try {
       uploadObject(call, callback)
-      //const response = new UploadObjectResponse()
-      //response.setSize(size)
-      //callback(null, response)
     } catch (e) {
       callback(e, null)
     }
@@ -30,6 +29,7 @@ class StorageServer implements IStorageServer {
     call: grpc.ServerUnaryCall<GetObjectURLRequest>,
     callback: grpc.sendUnaryData<GetObjectURLResponse>
   ): Promise<void> {
+    if (!auth(call)) return callback(new FonosAuthError(), null)
     try {
       const url = await getObjectURL(
         call.request.getBucket(),
