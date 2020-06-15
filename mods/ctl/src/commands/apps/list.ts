f4bbc84083a202d080f4c36c32bca976dbb114db
Pedro Sanders <sanderspedro@gmail.com>
2020-06-14 23:50:16 -0400

    Refactored

--- mods/ctl/src/commands/apps/list.ts
+++ mods/ctl/src/commands/apps/list.ts
@@ -1,29 +1,44 @@
-require('../../config')
-const AppManager = require('@fonos/appmanager')
+import '../../config'
+import AppManager from '@fonos/appmanager'
+import truncate from 'truncate'
+import { CLIError } from '@oclif/errors'
+import { Command, flags } from '@oclif/command'
+import moment from 'moment'
+import { prompt } from 'inquirer'
+import { View } from '../../../../core/src/server/protos/common_pb'
 const Table = require('easy-table')
-const truncate = require('truncate')
-const { CLIError } = require('@oclif/errors')
-const { Command, flags } = require('@oclif/command')
-const moment = require('moment')
-const inquirer = require('inquirer')
 
-class ListCommand extends Command {
+export default class ListCommand extends Command {
+  static aliases = ['apps:ls']
+  static description = `list registered applications
+  ...
+  List the registered applications
+  `
+  static flags = {
+    size: flags.integer({
+      char: 's',
+      default: 25,
+      description: 'number of result per page'
+    })
+  }
+
   async run () {
     const { flags } = this.parse(ListCommand)
     try {
       const appmanager = new AppManager()
       let firstBatch = true
       let pageToken = '0'
+      const view: View = View.BASIC
       const pageSize = flags.size
       while (true) {
         // Get a list
-        const result = await appmanager.listApps({ pageSize, pageToken })
+        const result = await appmanager.listApps({ pageSize, pageToken, view })
         const apps = result.getAppsList()
         pageToken = result.getNextPageToken()
 
         // Dont ask this if is the first time or empty data
         if (apps.length > 0 && !firstBatch) {
-          const answer = await inquirer.prompt([
+          const answer: any = await prompt([
             { name: 'q', message: 'More', type: 'confirm' }
           ])
           if (!answer.q) break
@@ -31,7 +46,7 @@ class ListCommand extends Command {
 
         const t = new Table()
 
-        apps.forEach(app => {
+        apps.forEach((app: any) => {
           t.cell('Name', app.getName())
           t.cell('Description', truncate(app.getDescription(), 32))
           t.cell('Created', moment(app.getCreateTime()).fromNow())
@@ -49,20 +64,3 @@ class ListCommand extends Command {
     }
   }
 }
-
-ListCommand.description = `list registered applications
-...
-List the registered applications
-`
-
-ListCommand.flags = {
-  size: flags.integer({
-    char: 's',
-    default: 25,
-    description: 'number of result per page'
-  })
-}
-
-ListCommand.aliases = ['apps:ls']
-
-module.exports = ListCommand
