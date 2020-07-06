63f3d0f18282ad2b231790ea994e1a5cb597cc06
Pedro Sanders <sanderspedro@gmail.com>
2020-07-05 20:47:07 -0400

    Fixed get command

---
+++ mods/ctl/src/commands/numbers/get.ts
@@ -0,0 +1,37 @@
+import '../../config'
+import Numbers from '@fonos/numbers'
+import { CLIError } from '@oclif/errors'
+import { Command } from '@oclif/command'
+import { cli } from 'cli-ux'
+import { render } from 'prettyjson'
+
+export default class GetCommand extends Command {
+  static description = 'get information about an existing number'
+  static args = [{ name: 'ref' }]
+
+  async run () {
+    const { args } = this.parse(GetCommand)
+
+    try {
+      const numbers = new Numbers()
+      cli.action.start(`Getting number ${args.ref}`)
+      const number = await numbers.getNumber(args.ref)
+
+      const jsonObj = {
+        Ref: number.getRef(),
+        'Provider Ref': number.getProviderRef(),
+        'E164 Numbe': number.getE164Number(),
+        'AOR Link': number.getAorLink() || '--',
+        'Ingress App': number.getIngressApp() || '--',
+        Created: number.getCreateTime(),
+        Updated: number.getUpdateTime()
+      }
+
+      await cli.wait(1000)
+      cli.action.stop('')
+      console.log(render(jsonObj, { noColor: true }))
+    } catch (e) {
+      throw new CLIError(e.message)
+    }
+  }
+}
