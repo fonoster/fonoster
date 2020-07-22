02024acebdd74caf199a7ef88385f181515fc0aa
Pedro Sanders <sanderspedro@gmail.com>
2020-07-21 21:34:11 -0400

    Improved example

--- examples/google-tts/index.js
+++ examples/google-tts/index.js
@@ -3,11 +3,16 @@
  * @since v1
  */
 const GoogleTTS = require('@fonos/googletts').default
+const path = require('path')
+const googleCredentials = {
+  projectId: 'clever-tube-275321',
+  keyFilename: path.join(__dirname, credentials.json)
+}
 
 module.exports = chan => {
   // Overwrite TTS engine
   const conf = chan._config
-  conf.tts = new GoogleTTS()
+  conf.tts = new GoogleTTS(googleCredentials)
   chan.config(conf)
   chan.say('Hello from google text to speech')
 }
