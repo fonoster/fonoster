430cc10469f9d3e888939dce7dbecafe1d1b8f51
Pedro Sanders <sanderspedro@gmail.com>
2020-11-15 08:41:19 -0500

    Refactoring

--- mods/certs/src/certs.ts
+++ mods/certs/src/certs.ts
@@ -4,11 +4,16 @@ import { forge } from 'acme-client'
 import { join } from 'path'
 import { homedir } from 'os'
 
+const btoa = require('btoa')
+
 const BASE_DIR = join(homedir(), '.fonos')
 const PATH_TO_SALT = join(BASE_DIR, 'jwt.salt')
 const PATH_TO_CONFIG = join(BASE_DIR, 'config')
 const ACCESS_KEY_ID = process.env.ACCESS_KEY_ID || 'fonos'
 const ISS = process.env.ISS || 'fonos'
+
+const getContent = (file: string) =>
+  btoa(fs.readFileSync(`${BASE_DIR}/${file}`).toString('utf-8'))
 
 if (!fs.existsSync(BASE_DIR)) fs.mkdirSync(BASE_DIR)
 
@@ -35,8 +40,27 @@ async function createAccessFile () {
   return config
 }
 
+async function createConfig (subject: string) {
+  try {
+    const config = JSON.parse(
+      fs.readFileSync(join(BASE_DIR, 'config')).toString('utf-8')
+    )
+    config.endpoint = subject
+    config.caCertificate = getContent('ca.crt')
+    config.clientCertificate = getContent('client.crt')
+    config.clientKey = getContent('client.key')
+
+    const content = JSON.stringify(config, null, '')
+    fs.mkdirSync(BASE_DIR, { recursive: true })
+    fs.writeFileSync(PATH_TO_CONFIG, content)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
 export {
   createAccessFile as default,
+  createConfig,
   getSalt,
   configExist,
   saltExist,
