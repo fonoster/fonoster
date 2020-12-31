5689db3db1a18db31a9ec07eec47325dbb55c83f
Pedro Sanders <sanderspedro@gmail.com>
2020-12-30 23:48:02 -0500

    Fixed typo

--- mods/agents/src/agents.ts
+++ mods/agents/src/agents.ts
@@ -80,7 +80,7 @@ class Agents extends FonosService {
   }
 
   /**
-   * Retrives a Agent by its reference.
+   * Retrives an Agent by its reference.
    *
    * @param {string} ref - Reference to Agent
    * @return {Promise<Object>} The agent
