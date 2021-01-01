1b9d6d5f3f92e3a16b059ed251361330b1dcffd5
Pedro Sanders <sanderspedro@gmail.com>
2021-01-01 17:02:01 -0500

    Removed class name

--- mods/core/src/common/fsutils.ts
+++ mods/core/src/common/fsutils.ts
@@ -10,23 +10,16 @@ import logger from '@fonos/logger'
 import policy from './bucket_policy'
 import { fsInstance } from './utils'
 
-export default async function updateBucketPolicy (bucket: string) {
-  try {
-    const fsConn = fsInstance()
-    const exists = await fsConn.bucketExists(bucket)
+export default async function (bucket: string) {
+  const fsConn = fsInstance()
+  const exists = await fsConn.bucketExists(bucket)
 
-    if (!exists) {
-      logger.log(
-        'verbose',
-        `@fonos/core fsutils [Creating storage and setting policy bucket: ${bucket}]`
-      )
-      await fsConn.makeBucket(bucket, 'us-west-1')
-      await fsConn.setBucketPolicy(bucket, policy(bucket))
-    }
-  } catch (err) {
-    throw err
+  if (!exists) {
+    logger.log(
+      'verbose',
+      `@fonos/core fsutils [Creating storage and setting policy bucket: ${bucket}]`
+    )
+    await fsConn.makeBucket(bucket, 'us-west-1')
+    await fsConn.setBucketPolicy(bucket, policy(bucket))
   }
 }
-
-updateBucketPolicy('apps')
-updateBucketPolicy('default')
