8bacb38c1f2539fbee7fbd0751c3fec72fc9cbaa
Richard Hernandez <richard.hernandez@externos.toc.cl>
2021-02-04 16:42:50 -0400

    Fixing naming convention in usermanger.proto

--- etc/gen_code_proto.sh
+++ etc/gen_code_proto.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 DIRNAME="$(cd "$(dirname "$0")"; pwd)"
 
-PROTOS=(appmanager common storage providers numbers domains agents usermanager)
+PROTOS=(appmanager common storage providers numbers domains agents)
 
 for proto in "${PROTOS[@]}"
 do
