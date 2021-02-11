046e0c7fa5bd448730d66453c5179bb9330f07d2
Pedro Sanders <sanderspedro@gmail.com>
2021-02-10 22:50:34 -0500

    Cleanup

--- mods/sdk/src/index.ts
+++ mods/sdk/src/index.ts
@@ -5,5 +5,8 @@ import Providers from '@fonos/providers'
 import Numbers from '@fonos/numbers'
 import Domains from '@fonos/domains'
 import Agents from '@fonos/agents'
+import { mute } from '@fonos/logger'
+
+mute()
 
 export { AppManager, CallManager, Storage, Providers, Numbers, Domains, Agents }
