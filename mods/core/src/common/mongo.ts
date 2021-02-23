1837e7239bc1d633a14a499d72f274a0541b5b7e
Richard Hernandez <richard.hernandez@externos.toc.cl>
2021-02-23 09:46:20 -0400

    Refactoring User model and validation

--- mods/core/src/common/mongo.ts
+++ mods/core/src/common/mongo.ts
@@ -4,11 +4,10 @@ import logger from '@fonos/logger'
 const host = process.env.DS_AUTH_HOST || 'localhost'
 const port = process.env.DS_AUTH_PORT || 27017
 const dbname = process.env.DS_AUTH_DB || 'fonos'
+
 const credentials = process.env.DS_AUTH_USERNAME && process.env.DS_SECRET  
     ? `${process.env.DS_AUTH_USERNAME}:${process.env.DS_SECRET}@` : ''
 export const db = `mongodb://${credentials}${host}:${port}/${dbname}`
-
-console.log('db:', db)
 
 export const mongoConnection = () => {
     mongoose
