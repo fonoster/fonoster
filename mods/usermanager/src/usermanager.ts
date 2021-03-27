dde5101c718d946628fcde6dc2cbd6fc4cdf07f0
Pedro Sanders <sanderspedro@gmail.com>
2021-03-27 11:51:46 -0400

    Code cleanup

--- mods/usermanager/src/usermanager.ts
+++ mods/usermanager/src/usermanager.ts
@@ -1,20 +1,20 @@
-import { FonosService, UserManagerService, UserManagerPB } from '@fonos/core'
+import { FonosService, UserManagerService, UserManagerPB } from "@fonos/core";
 
 interface CreateUserRequest {
-  firstName: string,
-  lastName: string,
-  email : string
+  firstName: string;
+  lastName: string;
+  email: string;
 }
 
 interface User {
-  firstName: string,
-  lastName: string,
-  email : string,
-  accessKeyId : string
-  role : string,
-  createTime : string,
-  updateTime : string,
-  status : string
+  firstName: string;
+  lastName: string;
+  email: string;
+  accessKeyId: string;
+  role: string;
+  createTime: string;
+  updateTime: string;
+  status: string;
 }
 
 /**
@@ -27,45 +27,44 @@ interface User {
  *
  * const Fonos = require('@fonos/sdk')
  * const users = new Fonos.UserManager()
- * 
+ *
  * TODO: Adde example
  */
 export default class UserManager extends FonosService {
-
   /**
    * Constructs a new AppManager Object.
    *
    * @see module:core:FonosService
    */
-  constructor (options?: any) {
-    super(UserManagerService.UserManagerClient, options)
-    super.init()
-    const { promisifyAll } = require('grpc-promise')
-    promisifyAll(super.getService(), { metadata: super.getMeta() })
+  constructor(options?: any) {
+    super(UserManagerService.UserManagerClient, options);
+    super.init();
+    const { promisifyAll } = require("grpc-promise");
+    promisifyAll(super.getService(), { metadata: super.getMeta() });
   }
 
-  async createUser (request: CreateUserRequest): Promise<User> {
+  async createUser(request: CreateUserRequest): Promise<User> {
     const user = new UserManagerPB.User();
-    user.setFirstName(request.firstName)
-    user.setLastName(request.lastName)
-    user.setEmail(request.email)
-    const req = new UserManagerPB.CreateUserRequest()
+    user.setFirstName(request.firstName);
+    user.setLastName(request.lastName);
+    user.setEmail(request.email);
+    const req = new UserManagerPB.CreateUserRequest();
     req.setUser(user);
 
-    const userFromDatabase =  await super
-    .getService()
-    .createUser()
-    .sendMessage(req)
+    const userFromDatabase = await super
+      .getService()
+      .createUser()
+      .sendMessage(req);
 
     return {
       firstName: userFromDatabase.getFirstName(),
       lastName: userFromDatabase.getLastName(),
-      email : userFromDatabase.getEmail(),
-      accessKeyId : userFromDatabase.getAccessKeyId(),
-      role : userFromDatabase.getRole(),
-      createTime : userFromDatabase.getCreateTime(),
-      updateTime : userFromDatabase.getUpdateTime(),
-      status : userFromDatabase.getStatus()
-    }
+      email: userFromDatabase.getEmail(),
+      accessKeyId: userFromDatabase.getAccessKeyId(),
+      role: userFromDatabase.getRole(),
+      createTime: userFromDatabase.getCreateTime(),
+      updateTime: userFromDatabase.getUpdateTime(),
+      status: userFromDatabase.getStatus()
+    };
   }
 }
