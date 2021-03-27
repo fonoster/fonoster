eb4addd0eb514d05e37f50773a38db0cfa14dcef
Pedro Sanders <sanderspedro@gmail.com>
2021-03-27 11:52:10 -0400

    Refactored

--- examples/dialog/src/talk.js
+++ examples/dialog/src/talk.js
@@ -1,32 +1,36 @@
-const dialogflow = require('@google-cloud/dialogflow')
-const uuid = require('uuid')
+const dialogflow = require("@google-cloud/dialogflow");
+const uuid = require("uuid");
 
 class Talk {
-    constructor(projectId, credentials) {
-        const sessionId = uuid.v4()
-        // Create a new session
-        this.sessionClient = new dialogflow.SessionsClient(credentials)
-        this.sessionPath = this.sessionClient.projectAgentSessionPath(projectId, sessionId)
-    }
+  constructor(projectId, credentials) {
+    const sessionId = uuid.v4();
+    // Create a new session
+    this.sessionClient = new dialogflow.SessionsClient(credentials);
+    this.sessionPath = this.sessionClient.projectAgentSessionPath(
+      projectId,
+      sessionId
+    );
+  }
 
-    findIntent(txt, callback) {
-        const request = {
-            session: this.sessionPath,
-            queryInput: {
-                text: {
-                    text: txt,
-                    languageCode: 'en-US',
-                },
-            },
+  findIntent(txt, callback) {
+    const request = {
+      session: this.sessionPath,
+      queryInput: {
+        text: {
+          text: txt,
+          languageCode: "en-US"
         }
-        process.nextTick(() => {
-            this.sessionClient.detectIntent(request)
-            .then(responses => {
-                callback(null, responses[0].queryResult)
-            })
-            .catch(e => callback(e))
+      }
+    };
+    process.nextTick(() => {
+      this.sessionClient
+        .detectIntent(request)
+        .then((responses) => {
+          callback(null, responses[0].queryResult);
         })
-    }
+        .catch((e) => callback(e));
+    });
+  }
 }
 
-module.exports = Talk
+module.exports = Talk;
