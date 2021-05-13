eda0e3bedfd56b91600e62272dd5872199db55d6
Pedro Sanders <sanderspedro@gmail.com>
2021-05-13 10:32:00 -0400

    Implemented most of the backend

--- mods/funcs/src/service/funcs.ts
+++ mods/funcs/src/service/funcs.ts
@@ -1,87 +1,234 @@
 import grpc from "grpc";
-import createApp from "./deploy_func";
-import listApps from "./list_funcs";
-import getApp from "./get_func";
-import deleteApp from "./delete_func";
-import {
-  App,
-  ListAppsRequest,
-  ListAppsResponse,
-  GetAppRequest,
-  CreateAppRequest,
-  UpdateAppRequest,
-  DeleteAppRequest
-} from "./protos/appmanager_pb";
 import {Empty} from "./protos/common_pb";
+import {
+  IFuncsService,
+  FuncsService,
+  IFuncsServer
+} from "./protos/funcs_grpc_pb";
+import {
+  CreateFuncRequest,
+  UpdateFuncRequest,
+  DeleteFuncRequest,
+  Func,
+  FuncLog,
+  GetFuncLogsRequest,
+  GetFuncRequest,
+  ListFuncsRequest,
+  ListFuncsResponse
+} from "./protos/funcs_pb";
+import {HttpBasicAuth, DefaultApi as FaaS} from "openfaas-client";
+import logger from "@fonos/logger";
+import {ErrorCodes, FonosError, FonosSubsysUnavailable} from "@fonos/errors";
 import {getAccessKeyId} from "@fonos/core";
 
-import {
-  IAppManagerService,
-  AppManagerService,
-  IAppManagerServer
-} from "./protos/appmanager_grpc_pb";
+const registryAuth = "cHNhbmRlcnM6RzR0MHA0cmQwIw==";
 
-class AppManagerServer implements IAppManagerServer {
-  async listApps(
-    call: grpc.ServerUnaryCall<ListAppsRequest>,
-    callback: grpc.sendUnaryData<ListAppsResponse>
+const auth = new HttpBasicAuth();
+auth.username = "admin";
+auth.password =
+  "N4iuzYIh2IZJ0ZPMoJQH1xOLlUIhOBvTvruXzjT4LIhD3Lfr4RbuFc7PPv8V8Uk";
+
+const faas = new FaaS();
+faas.setDefaultAuthentication(auth);
+faas.basePath = "http://faasd.fonoster.net:8080";
+
+const getFuncName = (accessKeyId: string, name: string) =>
+  `fn-${accessKeyId}-${name}`;
+
+interface FuncParameters {
+  func: Func;
+  registryAuth: string;
+  accessKeyId: string;
+  jwtSignature: string;
+}
+
+const prepareParameters = (params: FuncParameters) => {
+  const parameters = {
+    service: getFuncName(params.accessKeyId, params.func.getName()),
+    image: params.func.getImage(),
+    limits: {
+      memory: undefined,
+      cpu: undefined
+    },
+    requests: {
+      memory: undefined,
+      cpu: undefined
+    },
+    envProcess: "npm run start",
+    registryAuth,
+    labels: {
+      funcName: params.func.getName()
+    },
+    envVars: {
+      ACCESS_KEY_ID: params.accessKeyId,
+      JWT_SIGNATURE: params.jwtSignature
+    }
+  };
+  const limits = params.func.getLimits();
+  const requests = params.func.getRequests();
+
+  if (limits.getMemory()) parameters.limits.memory = limits.getMemory();
+  if (limits.getCpu()) parameters.limits.cpu = limits.getCpu();
+  if (requests.getMemory()) parameters.requests.memory = requests.getMemory();
+  if (requests.getCpu()) parameters.requests.cpu = requests.getCpu();
+
+  return parameters;
+};
+
+const rawFuncToFunc = (rawFunc:any) => {
+  const func = new Func();
+  func.setName(rawFunc.labels.funcName);
+  func.setImage(rawFunc.image);
+  func.setInvocationCount(rawFunc.invocationCount);
+  func.setReplicas(rawFunc.replicas);
+  func.setAvailableReplicas(rawFunc.availableReplicas);
+  return func;
+}
+
+class FuncsServer implements IFuncsServer {
+  getFuncLogs: grpc.handleServerStreamingCall<GetFuncLogsRequest, FuncLog>;
+
+  // See client-side for comments
+  async listFuncs(
+    call: grpc.ServerUnaryCall<ListFuncsRequest>,
+    callback: grpc.sendUnaryData<ListFuncsResponse>
+  ) {
+    if (!call.request.getPageToken()) callback(null, new ListFuncsResponse());
+    const accessKeyId = getAccessKeyId(call);
+    const list = (await faas.systemFunctionsGet()).response.body;
+    const rawFuncs = list.filter(
+      (f) => f.envVars.ACCESS_KEY_ID === accessKeyId
+    );
+
+    const funcs = rawFuncs.map(f => rawFuncToFunc(f));
+    const response = new ListFuncsResponse();
+    response.setFuncsList(funcs);
+    // No pagination need because the list of function is likely to be short
+    // response.setNextPageToken()
+
+    callback(null, response);
+  }
+
+  // See client-side for comments
+  async getFunc(
+    call: grpc.ServerUnaryCall<GetFuncRequest>,
+    callback: grpc.sendUnaryData<Func>
   ) {
     try {
-      const result = await listApps(
-        parseInt(call.request.getPageToken()),
-        call.request.getPageSize(),
-        getAccessKeyId(call)
-      );
-      const response = new ListAppsResponse();
-      response.setAppsList(result.apps);
-      if (result.pageToken) response.setNextPageToken("" + result.pageToken);
-      callback(null, response);
+      const list = (await faas.systemFunctionsGet()).response.body;
+      const accessKeyId = getAccessKeyId(call);
+      const rawFunction = list.filter(
+        (f) => f.name === getFuncName(accessKeyId, call.request.getRef())
+      )[0];
+
+      if (!rawFunction)
+        throw new FonosError(
+          `Function name ${call.request.getRef()} doesn't exist`,
+          ErrorCodes.NOT_FOUND
+        );
+
+      callback(null, rawFuncToFunc(rawFunction));
     } catch (e) {
       callback(e, null);
     }
   }
 
-  async getApp(
-    call: grpc.ServerUnaryCall<GetAppRequest>,
-    callback: grpc.sendUnaryData<App>
+  // See client-side for comments
+  async createFunc(
+    call: grpc.ServerUnaryCall<CreateFuncRequest>,
+    callback: grpc.sendUnaryData<Func>
   ) {
     try {
-      callback(null, await getApp(call.request.getRef(), getAccessKeyId(call)));
+      const parameters = prepareParameters({
+        func: call.request.getFunc(),
+        registryAuth,
+        accessKeyId: getAccessKeyId(call),
+        jwtSignature: "" // TODO
+      });
+      await faas.systemFunctionsPost(parameters);
+      callback(null, call.request.getFunc());
     } catch (e) {
-      callback(e, null);
+      if (e.response.body.includes("already exists")) {
+        callback(
+          new FonosError(
+            "Entity already exist",
+            ErrorCodes.ENTITY_ALREADY_EXIST
+          ),
+          null
+        );
+      } else if (e.response.statusCode === 400) {
+        callback(
+          new FonosError(e.response.body, ErrorCodes.INVALID_ARGUMENT),
+          null
+        );
+      } else if (e.response.statusCode === 401) {
+        callback(
+          new FonosSubsysUnavailable("Functions subsystem unavailable"),
+          null
+        );
+      }
+      callback(e.response, null);
     }
   }
 
-  async createApp(
-    call: grpc.ServerUnaryCall<CreateAppRequest>,
-    callback: grpc.sendUnaryData<App>
+  // See client-side for comments
+  async updateFunc(
+    call: grpc.ServerUnaryCall<UpdateFuncRequest>,
+    callback: grpc.sendUnaryData<Func>
   ) {
     try {
-      callback(
-        null,
-        await createApp(call.request.getApp(), getAccessKeyId(call))
-      );
+      const accessKeyId = getAccessKeyId(call);
+      const parameters = prepareParameters({
+        func: call.request.getFunc(),
+        registryAuth,
+        accessKeyId: accessKeyId,
+        jwtSignature: "" // TODO
+      });
+      await faas.systemFunctionsPut(parameters);
+      // Get result from the system
+      const list = (await faas.systemFunctionsGet()).response.body;
+      const rawFunction = list.filter(
+        (f) =>
+          f.name === getFuncName(accessKeyId, call.request.getFunc().getName())
+      )[0];
+     
+      callback(null, rawFuncToFunc(rawFunction));
     } catch (e) {
-      callback(e, null);
+      logger.error(e.message);
+      if (e.response.statusCode === 400) {
+        callback(
+          new FonosError(e.response.body, ErrorCodes.INVALID_ARGUMENT),
+          null
+        );
+      } else if (e.response.statusCode === 401) {
+        callback(
+          new FonosSubsysUnavailable("Functions subsystem unavailable"),
+          null
+        );
+      } else if (e.response.statusCode === 404) {
+        callback(new FonosError(e.response.body, ErrorCodes.NOT_FOUND), null);
+      }
+      callback(e.response, null);
     }
   }
 
-  updateApp(
-    call: grpc.ServerUnaryCall<UpdateAppRequest>,
-    callback: grpc.sendUnaryData<App>
-  ): void {}
-
-  async deleteApp(
-    call: grpc.ServerUnaryCall<DeleteAppRequest>,
+  // See client-side for comments
+  async deleteFunc(
+    call: grpc.ServerUnaryCall<DeleteFuncRequest>,
     callback: grpc.sendUnaryData<Empty>
   ) {
     try {
-      await deleteApp(call.request.getRef(), getAccessKeyId(call));
+      const accessKeyId = getAccessKeyId(call);
+      const functionName = getFuncName(accessKeyId, call.request.getRef());
+      await faas.systemFunctionsDelete({functionName});
       callback(null, new Empty());
     } catch (e) {
+      if (e.response.statusCode === 404) {
+        callback(new FonosError(e.response.body, ErrorCodes.NOT_FOUND), null);
+      }
       callback(e, null);
     }
   }
 }
 
-export {AppManagerServer as default, IAppManagerService, AppManagerService};
+export {FuncsServer as default, IFuncsService, FuncsService};
