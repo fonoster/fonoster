78405944c7079ab8e3fa680a6889d744ef0f8002
Pedro Sanders <sanderspedro@gmail.com>
2021-05-13 10:01:58 -0400

    Updated lock files and protobuf pbs

--- mods/funcs/src/service/protos/funcs_grpc_pb.js
+++ mods/funcs/src/service/protos/funcs_grpc_pb.js
@@ -7,36 +7,44 @@
 //
 // The Funcs proto contains the artificats for function administration
 // such as creation and deployment.
-'use strict';
-var grpc = require('grpc');
-var funcs_pb = require('./funcs_pb.js');
-var common_pb = require('./common_pb.js');
+"use strict";
+var grpc = require("grpc");
+var funcs_pb = require("./funcs_pb.js");
+var common_pb = require("./common_pb.js");
 
 function serialize_fonos_appmanager_v1alpha1_CreateFuncRequest(arg) {
   if (!(arg instanceof funcs_pb.CreateFuncRequest)) {
-    throw new Error('Expected argument of type fonos.appmanager.v1alpha1.CreateFuncRequest');
+    throw new Error(
+      "Expected argument of type fonos.appmanager.v1alpha1.CreateFuncRequest"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_fonos_appmanager_v1alpha1_CreateFuncRequest(buffer_arg) {
-  return funcs_pb.CreateFuncRequest.deserializeBinary(new Uint8Array(buffer_arg));
+  return funcs_pb.CreateFuncRequest.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_fonos_appmanager_v1alpha1_DeleteFuncRequest(arg) {
   if (!(arg instanceof funcs_pb.DeleteFuncRequest)) {
-    throw new Error('Expected argument of type fonos.appmanager.v1alpha1.DeleteFuncRequest');
+    throw new Error(
+      "Expected argument of type fonos.appmanager.v1alpha1.DeleteFuncRequest"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_fonos_appmanager_v1alpha1_DeleteFuncRequest(buffer_arg) {
-  return funcs_pb.DeleteFuncRequest.deserializeBinary(new Uint8Array(buffer_arg));
+  return funcs_pb.DeleteFuncRequest.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_fonos_appmanager_v1alpha1_Func(arg) {
   if (!(arg instanceof funcs_pb.Func)) {
-    throw new Error('Expected argument of type fonos.appmanager.v1alpha1.Func');
+    throw new Error("Expected argument of type fonos.appmanager.v1alpha1.Func");
   }
   return Buffer.from(arg.serializeBinary());
 }
@@ -47,7 +55,9 @@ function deserialize_fonos_appmanager_v1alpha1_Func(buffer_arg) {
 
 function serialize_fonos_appmanager_v1alpha1_FuncLog(arg) {
   if (!(arg instanceof funcs_pb.FuncLog)) {
-    throw new Error('Expected argument of type fonos.appmanager.v1alpha1.FuncLog');
+    throw new Error(
+      "Expected argument of type fonos.appmanager.v1alpha1.FuncLog"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
@@ -58,18 +68,24 @@ function deserialize_fonos_appmanager_v1alpha1_FuncLog(buffer_arg) {
 
 function serialize_fonos_appmanager_v1alpha1_GetFuncLogsRequest(arg) {
   if (!(arg instanceof funcs_pb.GetFuncLogsRequest)) {
-    throw new Error('Expected argument of type fonos.appmanager.v1alpha1.GetFuncLogsRequest');
+    throw new Error(
+      "Expected argument of type fonos.appmanager.v1alpha1.GetFuncLogsRequest"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_fonos_appmanager_v1alpha1_GetFuncLogsRequest(buffer_arg) {
-  return funcs_pb.GetFuncLogsRequest.deserializeBinary(new Uint8Array(buffer_arg));
+  return funcs_pb.GetFuncLogsRequest.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_fonos_appmanager_v1alpha1_GetFuncRequest(arg) {
   if (!(arg instanceof funcs_pb.GetFuncRequest)) {
-    throw new Error('Expected argument of type fonos.appmanager.v1alpha1.GetFuncRequest');
+    throw new Error(
+      "Expected argument of type fonos.appmanager.v1alpha1.GetFuncRequest"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
@@ -80,40 +96,52 @@ function deserialize_fonos_appmanager_v1alpha1_GetFuncRequest(buffer_arg) {
 
 function serialize_fonos_appmanager_v1alpha1_ListFuncsRequest(arg) {
   if (!(arg instanceof funcs_pb.ListFuncsRequest)) {
-    throw new Error('Expected argument of type fonos.appmanager.v1alpha1.ListFuncsRequest');
+    throw new Error(
+      "Expected argument of type fonos.appmanager.v1alpha1.ListFuncsRequest"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_fonos_appmanager_v1alpha1_ListFuncsRequest(buffer_arg) {
-  return funcs_pb.ListFuncsRequest.deserializeBinary(new Uint8Array(buffer_arg));
+  return funcs_pb.ListFuncsRequest.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_fonos_appmanager_v1alpha1_ListFuncsResponse(arg) {
   if (!(arg instanceof funcs_pb.ListFuncsResponse)) {
-    throw new Error('Expected argument of type fonos.appmanager.v1alpha1.ListFuncsResponse');
+    throw new Error(
+      "Expected argument of type fonos.appmanager.v1alpha1.ListFuncsResponse"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_fonos_appmanager_v1alpha1_ListFuncsResponse(buffer_arg) {
-  return funcs_pb.ListFuncsResponse.deserializeBinary(new Uint8Array(buffer_arg));
+  return funcs_pb.ListFuncsResponse.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_fonos_appmanager_v1alpha1_UpdateFuncRequest(arg) {
   if (!(arg instanceof funcs_pb.UpdateFuncRequest)) {
-    throw new Error('Expected argument of type fonos.appmanager.v1alpha1.UpdateFuncRequest');
+    throw new Error(
+      "Expected argument of type fonos.appmanager.v1alpha1.UpdateFuncRequest"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_fonos_appmanager_v1alpha1_UpdateFuncRequest(buffer_arg) {
-  return funcs_pb.UpdateFuncRequest.deserializeBinary(new Uint8Array(buffer_arg));
+  return funcs_pb.UpdateFuncRequest.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_fonos_common_v1alpha1_Empty(arg) {
   if (!(arg instanceof common_pb.Empty)) {
-    throw new Error('Expected argument of type fonos.common.v1alpha1.Empty');
+    throw new Error("Expected argument of type fonos.common.v1alpha1.Empty");
   }
   return Buffer.from(arg.serializeBinary());
 }
@@ -122,11 +150,10 @@ function deserialize_fonos_common_v1alpha1_Empty(buffer_arg) {
   return common_pb.Empty.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-
-var FuncsService = exports.FuncsService = {
+var FuncsService = (exports.FuncsService = {
   // Shows a list of user functions
-listFuncs: {
-    path: '/fonos.appmanager.v1alpha1.Funcs/ListFuncs',
+  listFuncs: {
+    path: "/fonos.appmanager.v1alpha1.Funcs/ListFuncs",
     requestStream: false,
     responseStream: false,
     requestType: funcs_pb.ListFuncsRequest,
@@ -134,11 +161,11 @@ listFuncs: {
     requestSerialize: serialize_fonos_appmanager_v1alpha1_ListFuncsRequest,
     requestDeserialize: deserialize_fonos_appmanager_v1alpha1_ListFuncsRequest,
     responseSerialize: serialize_fonos_appmanager_v1alpha1_ListFuncsResponse,
-    responseDeserialize: deserialize_fonos_appmanager_v1alpha1_ListFuncsResponse,
+    responseDeserialize: deserialize_fonos_appmanager_v1alpha1_ListFuncsResponse
   },
   // Gets a function by namem
-getFunc: {
-    path: '/fonos.appmanager.v1alpha1.Funcs/GetFunc',
+  getFunc: {
+    path: "/fonos.appmanager.v1alpha1.Funcs/GetFunc",
     requestStream: false,
     responseStream: false,
     requestType: funcs_pb.GetFuncRequest,
@@ -146,11 +173,11 @@ getFunc: {
     requestSerialize: serialize_fonos_appmanager_v1alpha1_GetFuncRequest,
     requestDeserialize: deserialize_fonos_appmanager_v1alpha1_GetFuncRequest,
     responseSerialize: serialize_fonos_appmanager_v1alpha1_Func,
-    responseDeserialize: deserialize_fonos_appmanager_v1alpha1_Func,
+    responseDeserialize: deserialize_fonos_appmanager_v1alpha1_Func
   },
   // Orders the functions subsystem to download and deploy a function
-createFunc: {
-    path: '/fonos.appmanager.v1alpha1.Funcs/CreateFunc',
+  createFunc: {
+    path: "/fonos.appmanager.v1alpha1.Funcs/CreateFunc",
     requestStream: false,
     responseStream: false,
     requestType: funcs_pb.CreateFuncRequest,
@@ -158,11 +185,11 @@ createFunc: {
     requestSerialize: serialize_fonos_appmanager_v1alpha1_CreateFuncRequest,
     requestDeserialize: deserialize_fonos_appmanager_v1alpha1_CreateFuncRequest,
     responseSerialize: serialize_fonos_appmanager_v1alpha1_Func,
-    responseDeserialize: deserialize_fonos_appmanager_v1alpha1_Func,
+    responseDeserialize: deserialize_fonos_appmanager_v1alpha1_Func
   },
   // Updates a function, keeping metrics intact
-updateFunc: {
-    path: '/fonos.appmanager.v1alpha1.Funcs/UpdateFunc',
+  updateFunc: {
+    path: "/fonos.appmanager.v1alpha1.Funcs/UpdateFunc",
     requestStream: false,
     responseStream: false,
     requestType: funcs_pb.UpdateFuncRequest,
@@ -170,11 +197,11 @@ updateFunc: {
     requestSerialize: serialize_fonos_appmanager_v1alpha1_UpdateFuncRequest,
     requestDeserialize: deserialize_fonos_appmanager_v1alpha1_UpdateFuncRequest,
     responseSerialize: serialize_fonos_appmanager_v1alpha1_Func,
-    responseDeserialize: deserialize_fonos_appmanager_v1alpha1_Func,
+    responseDeserialize: deserialize_fonos_appmanager_v1alpha1_Func
   },
   // Peforms a hard delete of the function
-deleteFunc: {
-    path: '/fonos.appmanager.v1alpha1.Funcs/DeleteFunc',
+  deleteFunc: {
+    path: "/fonos.appmanager.v1alpha1.Funcs/DeleteFunc",
     requestStream: false,
     responseStream: false,
     requestType: funcs_pb.DeleteFuncRequest,
@@ -182,11 +209,11 @@ deleteFunc: {
     requestSerialize: serialize_fonos_appmanager_v1alpha1_DeleteFuncRequest,
     requestDeserialize: deserialize_fonos_appmanager_v1alpha1_DeleteFuncRequest,
     responseSerialize: serialize_fonos_common_v1alpha1_Empty,
-    responseDeserialize: deserialize_fonos_common_v1alpha1_Empty,
+    responseDeserialize: deserialize_fonos_common_v1alpha1_Empty
   },
   // Peforms a hard delete of the function
-getFuncLogs: {
-    path: '/fonos.appmanager.v1alpha1.Funcs/GetFuncLogs',
+  getFuncLogs: {
+    path: "/fonos.appmanager.v1alpha1.Funcs/GetFuncLogs",
     requestStream: false,
     responseStream: true,
     requestType: funcs_pb.GetFuncLogsRequest,
@@ -194,8 +221,8 @@ getFuncLogs: {
     requestSerialize: serialize_fonos_appmanager_v1alpha1_GetFuncLogsRequest,
     requestDeserialize: deserialize_fonos_appmanager_v1alpha1_GetFuncLogsRequest,
     responseSerialize: serialize_fonos_appmanager_v1alpha1_FuncLog,
-    responseDeserialize: deserialize_fonos_appmanager_v1alpha1_FuncLog,
-  },
-};
+    responseDeserialize: deserialize_fonos_appmanager_v1alpha1_FuncLog
+  }
+});
 
 exports.FuncsClient = grpc.makeGenericClientConstructor(FuncsService);
--- mods/usermanager/src/service/protos/usermanager_grpc_pb.js
+++ mods/usermanager/src/service/protos/usermanager_grpc_pb.js
@@ -7,14 +7,14 @@
 //
 // The UserMananger proto contains the artificats for Users administration
 // such as creation and deployment.
-'use strict';
-var grpc = require('grpc');
-var usermanager_pb = require('./usermanager_pb.js');
-var common_pb = require('./common_pb.js');
+"use strict";
+var grpc = require("grpc");
+var usermanager_pb = require("./usermanager_pb.js");
+var common_pb = require("./common_pb.js");
 
 function serialize_fonos_common_v1alpha1_Empty(arg) {
   if (!(arg instanceof common_pb.Empty)) {
-    throw new Error('Expected argument of type fonos.common.v1alpha1.Empty');
+    throw new Error("Expected argument of type fonos.common.v1alpha1.Empty");
   }
   return Buffer.from(arg.serializeBinary());
 }
@@ -25,73 +25,99 @@ function deserialize_fonos_common_v1alpha1_Empty(buffer_arg) {
 
 function serialize_fonos_usermanager_v1alpha1_CreateUserRequest(arg) {
   if (!(arg instanceof usermanager_pb.CreateUserRequest)) {
-    throw new Error('Expected argument of type fonos.usermanager.v1alpha1.CreateUserRequest');
+    throw new Error(
+      "Expected argument of type fonos.usermanager.v1alpha1.CreateUserRequest"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_fonos_usermanager_v1alpha1_CreateUserRequest(buffer_arg) {
-  return usermanager_pb.CreateUserRequest.deserializeBinary(new Uint8Array(buffer_arg));
+  return usermanager_pb.CreateUserRequest.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_fonos_usermanager_v1alpha1_DeleteUserRequest(arg) {
   if (!(arg instanceof usermanager_pb.DeleteUserRequest)) {
-    throw new Error('Expected argument of type fonos.usermanager.v1alpha1.DeleteUserRequest');
+    throw new Error(
+      "Expected argument of type fonos.usermanager.v1alpha1.DeleteUserRequest"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_fonos_usermanager_v1alpha1_DeleteUserRequest(buffer_arg) {
-  return usermanager_pb.DeleteUserRequest.deserializeBinary(new Uint8Array(buffer_arg));
+  return usermanager_pb.DeleteUserRequest.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_fonos_usermanager_v1alpha1_GetUserRequest(arg) {
   if (!(arg instanceof usermanager_pb.GetUserRequest)) {
-    throw new Error('Expected argument of type fonos.usermanager.v1alpha1.GetUserRequest');
+    throw new Error(
+      "Expected argument of type fonos.usermanager.v1alpha1.GetUserRequest"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_fonos_usermanager_v1alpha1_GetUserRequest(buffer_arg) {
-  return usermanager_pb.GetUserRequest.deserializeBinary(new Uint8Array(buffer_arg));
+  return usermanager_pb.GetUserRequest.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_fonos_usermanager_v1alpha1_ListUsersRequest(arg) {
   if (!(arg instanceof usermanager_pb.ListUsersRequest)) {
-    throw new Error('Expected argument of type fonos.usermanager.v1alpha1.ListUsersRequest');
+    throw new Error(
+      "Expected argument of type fonos.usermanager.v1alpha1.ListUsersRequest"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_fonos_usermanager_v1alpha1_ListUsersRequest(buffer_arg) {
-  return usermanager_pb.ListUsersRequest.deserializeBinary(new Uint8Array(buffer_arg));
+  return usermanager_pb.ListUsersRequest.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_fonos_usermanager_v1alpha1_ListUsersResponse(arg) {
   if (!(arg instanceof usermanager_pb.ListUsersResponse)) {
-    throw new Error('Expected argument of type fonos.usermanager.v1alpha1.ListUsersResponse');
+    throw new Error(
+      "Expected argument of type fonos.usermanager.v1alpha1.ListUsersResponse"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_fonos_usermanager_v1alpha1_ListUsersResponse(buffer_arg) {
-  return usermanager_pb.ListUsersResponse.deserializeBinary(new Uint8Array(buffer_arg));
+  return usermanager_pb.ListUsersResponse.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_fonos_usermanager_v1alpha1_UpdateUserRequest(arg) {
   if (!(arg instanceof usermanager_pb.UpdateUserRequest)) {
-    throw new Error('Expected argument of type fonos.usermanager.v1alpha1.UpdateUserRequest');
+    throw new Error(
+      "Expected argument of type fonos.usermanager.v1alpha1.UpdateUserRequest"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_fonos_usermanager_v1alpha1_UpdateUserRequest(buffer_arg) {
-  return usermanager_pb.UpdateUserRequest.deserializeBinary(new Uint8Array(buffer_arg));
+  return usermanager_pb.UpdateUserRequest.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_fonos_usermanager_v1alpha1_User(arg) {
   if (!(arg instanceof usermanager_pb.User)) {
-    throw new Error('Expected argument of type fonos.usermanager.v1alpha1.User');
+    throw new Error(
+      "Expected argument of type fonos.usermanager.v1alpha1.User"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
@@ -100,11 +126,10 @@ function deserialize_fonos_usermanager_v1alpha1_User(buffer_arg) {
   return usermanager_pb.User.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-
-var UserManagerService = exports.UserManagerService = {
+var UserManagerService = (exports.UserManagerService = {
   // Lists user Userlications
-listUsers: {
-    path: '/fonos.usermanager.v1alpha1.UserManager/ListUsers',
+  listUsers: {
+    path: "/fonos.usermanager.v1alpha1.UserManager/ListUsers",
     requestStream: false,
     responseStream: false,
     requestType: usermanager_pb.ListUsersRequest,
@@ -112,11 +137,11 @@ listUsers: {
     requestSerialize: serialize_fonos_usermanager_v1alpha1_ListUsersRequest,
     requestDeserialize: deserialize_fonos_usermanager_v1alpha1_ListUsersRequest,
     responseSerialize: serialize_fonos_usermanager_v1alpha1_ListUsersResponse,
-    responseDeserialize: deserialize_fonos_usermanager_v1alpha1_ListUsersResponse,
+    responseDeserialize: deserialize_fonos_usermanager_v1alpha1_ListUsersResponse
   },
   // Gets User with the User-name
-getUser: {
-    path: '/fonos.usermanager.v1alpha1.UserManager/GetUser',
+  getUser: {
+    path: "/fonos.usermanager.v1alpha1.UserManager/GetUser",
     requestStream: false,
     responseStream: false,
     requestType: usermanager_pb.GetUserRequest,
@@ -124,10 +149,10 @@ getUser: {
     requestSerialize: serialize_fonos_usermanager_v1alpha1_GetUserRequest,
     requestDeserialize: deserialize_fonos_usermanager_v1alpha1_GetUserRequest,
     responseSerialize: serialize_fonos_usermanager_v1alpha1_User,
-    responseDeserialize: deserialize_fonos_usermanager_v1alpha1_User,
+    responseDeserialize: deserialize_fonos_usermanager_v1alpha1_User
   },
   createUser: {
-    path: '/fonos.usermanager.v1alpha1.UserManager/CreateUser',
+    path: "/fonos.usermanager.v1alpha1.UserManager/CreateUser",
     requestStream: false,
     responseStream: false,
     requestType: usermanager_pb.CreateUserRequest,
@@ -135,11 +160,11 @@ getUser: {
     requestSerialize: serialize_fonos_usermanager_v1alpha1_CreateUserRequest,
     requestDeserialize: deserialize_fonos_usermanager_v1alpha1_CreateUserRequest,
     responseSerialize: serialize_fonos_usermanager_v1alpha1_User,
-    responseDeserialize: deserialize_fonos_usermanager_v1alpha1_User,
+    responseDeserialize: deserialize_fonos_usermanager_v1alpha1_User
   },
   // Updates an User resource
-updateUser: {
-    path: '/fonos.usermanager.v1alpha1.UserManager/UpdateUser',
+  updateUser: {
+    path: "/fonos.usermanager.v1alpha1.UserManager/UpdateUser",
     requestStream: false,
     responseStream: false,
     requestType: usermanager_pb.UpdateUserRequest,
@@ -147,11 +172,11 @@ updateUser: {
     requestSerialize: serialize_fonos_usermanager_v1alpha1_UpdateUserRequest,
     requestDeserialize: deserialize_fonos_usermanager_v1alpha1_UpdateUserRequest,
     responseSerialize: serialize_fonos_usermanager_v1alpha1_User,
-    responseDeserialize: deserialize_fonos_usermanager_v1alpha1_User,
+    responseDeserialize: deserialize_fonos_usermanager_v1alpha1_User
   },
   // Peforms a hard delete of the User resource
-deleteUser: {
-    path: '/fonos.usermanager.v1alpha1.UserManager/DeleteUser',
+  deleteUser: {
+    path: "/fonos.usermanager.v1alpha1.UserManager/DeleteUser",
     requestStream: false,
     responseStream: false,
     requestType: usermanager_pb.DeleteUserRequest,
@@ -159,8 +184,10 @@ deleteUser: {
     requestSerialize: serialize_fonos_usermanager_v1alpha1_DeleteUserRequest,
     requestDeserialize: deserialize_fonos_usermanager_v1alpha1_DeleteUserRequest,
     responseSerialize: serialize_fonos_common_v1alpha1_Empty,
-    responseDeserialize: deserialize_fonos_common_v1alpha1_Empty,
-  },
-};
+    responseDeserialize: deserialize_fonos_common_v1alpha1_Empty
+  }
+});
 
-exports.UserManagerClient = grpc.makeGenericClientConstructor(UserManagerService);
+exports.UserManagerClient = grpc.makeGenericClientConstructor(
+  UserManagerService
+);
