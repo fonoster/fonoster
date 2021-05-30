48c1c82a326b49efa86854f841388f93f2bf80eb
Pedro Sanders <sanderspedro@gmail.com>
2021-05-29 21:31:00 -0400

    Refactoring numbers module

--- etc/gen_code_proto.sh
+++ etc/gen_code_proto.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env sh
 DIRNAME="$(cd "$(dirname "$0")"; pwd)"
   
-PROTOS=(agents appmanager callmanager domains storage numbers providers funcs auth secrets)
+PROTOS=(agents callmanager domains storage numbers providers funcs auth secrets)
 
 for proto in "${PROTOS[@]}"
 do
   grpc_tools_node_protoc -I=. /$DIRNAME/../mods/core/src/protos/common.proto \
     -I=. /$DIRNAME/../mods/${proto}/src/protos/${proto}.proto \
     --proto_path=$DIRNAME/../mods/core/src/protos \
-    --proto_path=$DIRNAME/../mods/appmanager/src/protos \
     --proto_path=$DIRNAME/../mods/${proto}/src/protos \
     --js_out=import_style=commonjs,binary:$DIRNAME/../mods/${proto}/src/service/protos \
     --grpc_out=$DIRNAME/../mods/${proto}/src/service/protos \
@@ -18,14 +17,10 @@ do
   grpc_tools_node_protoc -I=. /$DIRNAME/../mods/core/src/protos/common.proto \
     -I=. /$DIRNAME/../mods/${proto}/src/protos/${proto}.proto \
     --proto_path=$DIRNAME/../mods/core/src/protos \
-    --proto_path=$DIRNAME/../mods/appmanager/src/protos \
     --proto_path=$DIRNAME/../mods/${proto}/src/protos \
     --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
     --ts_out=$DIRNAME/../mods/${proto}/src/service/protos
 done
-
-# The numbers services also requires appmanager stubs
-cp $DIRNAME/../mods/appmanager/src/service/protos/* $DIRNAME/../mods/numbers/src/service/protos
 
 # Cleaning callmanager and auth stubs
 rm $DIRNAME/../mods/callmanager/src/service/protos/common_*
