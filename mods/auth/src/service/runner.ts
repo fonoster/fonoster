8b69b337c77dff8774b489f8f0a8a3de63ec0c10
Pedro Sanders <sanderspedro@gmail.com>
2021-06-06 17:14:40 -0400

    Implemented session token validation

--- mods/auth/src/service/runner.ts
+++ mods/auth/src/service/runner.ts
@@ -2,14 +2,44 @@
 import AuthServer from "./auth";
 import {AuthService} from "./protos/auth_grpc_pb";
 import {runServices} from "@fonos/common";
+import logger from "@fonos/logger";
+import express from "express";
+const app = express()
+import Auth from "../utils/auth_utils";
+import JWT from "../utils/jwt";
+import { getSalt } from "@fonos/certs";
+const authenticator = new Auth(new JWT());
 
-const services = [
-  {
-    name: "auth",
-    version: "v1alpha1",
-    service: AuthService,
-    server: new AuthServer()
+app.get('/session_auth', async(req, res) => {
+  const sessionToken = req.headers["x-session-token"];
+  const result = await authenticator.validateToken(
+    {accessToken: sessionToken},
+    getSalt()
+  );
+
+  if (!sessionToken || result.isValid === false) {
+    res.status(401);
+    res.send("Unauthorized");
+    return;
   }
-];
 
-runServices(services, []);
+  res.status(200);
+  res.send("Access granted")
+})
+
+// First starting the http 1.1 auth endpoint
+app.listen(3000, () => {
+  logger.info(`starting simple authentication service @ ${3000}`);
+
+  const services = [
+    {
+      name: "auth",
+      version: "v1alpha1",
+      service: AuthService,
+      server: new AuthServer()
+    }
+  ];
+  
+  runServices(services, []);
+})
+
