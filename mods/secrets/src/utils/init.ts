bbad490b89cfea9323213b54654b1c77406ad1e2
Pedro Sanders <sanderspedro@gmail.com>
2021-06-11 22:45:29 -0400

    Automating vault initialization

--- mods/secrets/src/utils/init.ts
+++ mods/secrets/src/utils/init.ts
@@ -7,8 +7,8 @@ const vault = Vault({
 
 // init vault server
 vault
-  .init({ secret_shares: 1, secret_threshold: 1 })
-  .then(async(result) => {
+  .init({secret_shares: 1, secret_threshold: 1})
+  .then(async (result) => {
     console.log("Initialized vault");
     const keys = result.keys;
     // set token for all following requests
@@ -20,23 +20,23 @@ vault
     console.log("token: " + result.root_token);
 
     // unseal vault server
-    await vault.unseal({ secret_shares: 1, key: keys[0] });
+    await vault.unseal({secret_shares: 1, key: keys[0]});
     console.log("Vault unsealed");
 
     // TODO: Adding initial policy
-    // vault policy write fonos-policy vault_policy.hcl 
+    // vault policy write fonos-policy vault_policy.hcl
 
     // TODO: Enable secret engine
     // vault secrets enable -path=secret kv
- 
+
     // WARNING: Enabling app role is not yet working
     // Enabling approle
     // vault auth enable approle
-    console.log("Enabling authentication method approle")
+    console.log("Enabling authentication method approle");
     await vault.enableAuth({
       mount_point: "approle",
-      type: 'approle',
-      description: 'Approle auth',
+      type: "approle",
+      description: "Approle auth"
     });
   })
   .catch((e) => {
