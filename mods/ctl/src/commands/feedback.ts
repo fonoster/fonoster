5440af47c7f1b85c71dd6c7af286fc10a9aa5f02
Pedro Sanders <sanderspedro@gmail.com>
2021-06-15 21:40:24 -0400

    Added feedback and bug commands

---
+++ mods/ctl/src/commands/feedback.ts
@@ -0,0 +1,14 @@
+import "../config";
+import cli from 'cli-ux'
+import {Command} from "@oclif/command";
+
+export default class extends Command {
+  static description = `let'us know how we're doing
+  ...
+  Help us improve by providing some feedback
+  `;
+
+  async run() {
+    await cli.open("https://docs.google.com/forms/d/e/1FAIpQLSd1G2ahRYqkbksOvz7XhNHfSLepUh3KzRHsXh2HXfZr68nhtQ/viewform?vc=0&c=0&w=1&flr=0");
+  }
+}
