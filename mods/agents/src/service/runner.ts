dd0bc80edc7314b97afe8e18fc7291cabfb49424
Pedro Sanders <sanderspedro@gmail.com>
2021-06-17 20:33:09 -0400

    Fixed runner and healthcheck aliases

--- mods/agents/src/service/runner.ts
+++ mods/agents/src/service/runner.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import AgentsServer from "./service/agents";
-import {AgentsService} from "./service/protos/agents_grpc_pb";
+import AgentsServer from "./agents";
+import {AgentsService} from "./protos/agents_grpc_pb";
 import {AuthMiddleware} from "@fonos/auth";
 import {getSalt} from "@fonos/certs";
 import {runServices} from "@fonos/common";
