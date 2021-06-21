ce880da099adbbccfa04c157831a76ee265aa9ea
Pedro Sanders <sanderspedro@gmail.com>
2021-06-20 20:04:04 -0400

    Added figlet

---
+++ mods/ctl/src/help.ts
@@ -0,0 +1,22 @@
+import Help from '@oclif/plugin-help';
+const figlet = require('figlet');
+
+export default class MyHelpClass extends Help {
+  showHelp(args: string[]) {
+    // Only shouw the figlet if user ask for help at the root
+    // level.
+    if (args.length === 0 ||
+      (args.length === 1 && (
+        args[0].includes("help") || args[0].includes("-h")))) {
+      console.log("\x1b[33m");
+      console.log(figlet.textSync('Project Fonos', {
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 60,
+        whitespaceBreak: true
+      }));
+      console.log("\x1b[0m");
+    }
+    super.showHelp(args);
+  }
+}
