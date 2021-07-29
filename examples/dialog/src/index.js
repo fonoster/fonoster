08d58838280d950d3012529cfc47de4180eed934
Raul Sanchez <rsanchez@evopointsolution.com>
2021-07-29 11:05:08 -0400

    Updating Fonos voice application examples

--- examples/dialog/src/index.js
+++ examples/dialog/src/index.js
@@ -1,4 +1,22 @@
-const { listen, talk, tts, asr } = require("./config");
+/*
+ * Copyright (C) 2021 by Fonoster Inc (https://fonoster.com)
+ * http://github.com/fonoster/fonos
+ *
+ * This file is part of Project Fonos
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const {listen, talk, tts, asr} = require("./config");
 
 module.exports = (chan) => {
   // Setting up Google TTS
@@ -36,7 +54,7 @@ module.exports = (chan) => {
       // Lets try again!
       noSpeechCount = 0;
       // There was no input
-      chan.say(`Sorry, I didn't quite get that. Can you say again?`);
+      chan.say("Sorry, I didn't quite get that. Can you say again?");
     }
   }
 };
--- examples/presidential-poll/index.js
+++ examples/presidential-poll/index.js
@@ -1,5 +1,23 @@
-const { VoiceServer } = require("@fonos/voice");
-const { tts } = require("./config");
+/*
+ * Copyright (C) 2021 by Fonoster Inc (https://fonoster.com)
+ * http://github.com/fonoster/fonos
+ *
+ * This file is part of Project Fonos
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const {VoiceServer} = require("@fonos/voice");
+const {tts} = require("./config");
 
 const config = {
   finishOnKey: "*",
@@ -12,9 +30,9 @@ const voiceServerConfig = {
   base: ""
 };
 const candidates = {
-  1: { name: "Donald Trump" },
-  2: { name: "Hillary Clinton" },
-  3: { name: "Beny Sanders" }
+  1: {name: "Donald Trump"},
+  2: {name: "Hillary Clinton"},
+  3: {name: "Beny Sanders"}
 };
 
 /**
@@ -22,7 +40,7 @@ const candidates = {
  */
 const voiceServer = new VoiceServer(voiceServerConfig);
 
-//You can use another TTS plugin as MaryTTS
+// You can use another TTS plugin as MaryTTS
 voiceServer.use(tts);
 
 voiceServer.listen(async (req, res) => {
