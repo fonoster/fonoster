59f4c92d1b38de5f070d018462bb40931ec6892b
Pedro Sanders <sanderspedro@gmail.com>
2021-08-25 17:03:32 -0400

    Cleanup

--- mods/common/src/env_is_set.ts
+++ mods/common/src/env_is_set.ts
@@ -1,7 +1,8 @@
 import logger from "@fonos/logger";
 
 export default function assertEnvIsSet(name: string) {
- const register = require(process.env.SERVICES_ENVS || "/home/fonos/service_envs.json");
+  const register = require(process.env.SERVICES_ENVS ||
+    "/home/fonos/service_envs.json");
   const services = register.filter((service: any) => service.module === name);
   for (let value of services) {
     value.env.forEach(function (variable: string) {
