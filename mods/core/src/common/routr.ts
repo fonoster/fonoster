f204d84ed639fdefb18a953a7c51ed4788dcb293
Pedro Sanders <sanderspedro@gmail.com>
2021-10-19 12:56:26 -0400

    Added new envs

--- mods/core/src/common/routr.ts
+++ mods/core/src/common/routr.ts
@@ -1,8 +1,7 @@
 import RoutrClient from "./routr_client";
 
-const host = process.env.SIPPROXY_API_HOST 
-  || process.env.SIPPROXY_HOST
-  || "localhost";
+const host =
+  process.env.SIPPROXY_API_HOST || process.env.SIPPROXY_HOST || "localhost";
 const port = process.env.SIPPROXY_API_PORT || "4567";
 const username = process.env.SIPPROXY_API_USERNAME || "admin";
 const secret = process.env.SIPPROXY_API_SECRET || "changeit";
