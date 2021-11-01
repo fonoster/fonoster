61f86d44bc5e20ad6364907854bf741010026fb1
Pedro Sanders <psanders@fonoster.com>
2021-11-01 00:09:40 -0400

    Update copyright.js

--- etc/copyright.js
+++ etc/copyright.js
@@ -1,8 +1,8 @@
 /*
  * Copyright (C) <%= YEAR %> by Fonoster Inc (https://fonoster.com)
- * http://github.com/fonoster/fonos
+ * http://github.com/fonoster/fonoster
  *
- * This file is part of Project Fonos
+ * This file is part of Fonoster
  *
  * Licensed under the MIT License (the "License");
  * you may not use this file except in compliance with
