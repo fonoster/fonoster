e2ee65c5b69480e43175af152c491ea00c4401a1
Pedro Sanders <sanderspedro@gmail.com>
2021-11-01 23:47:17 -0400

    Fixed duplicated docs

--- .scripts/gen_docs.js
+++ .scripts/gen_docs.js
@@ -4,27 +4,33 @@ const fs = require("fs");
 const path = require("path");
 
 /* input and output paths */
-const inputFile = path.join(__dirname, "../mods/**/dist/client/*.js");
 const outputDir = path.join(__dirname, "../docs/reference");
 
-/* get template data */
-const templateData = jsdoc2md.getTemplateDataSync({ files: inputFile });
+function generate(inputFile) {
+  /* get template data */
+  const templateData = jsdoc2md.getTemplateDataSync({ files: inputFile });
 
-/* reduce templateData to an array of class names */
-const classNames = templateData.reduce((names, identifier) => {
-  if (identifier.kind === "class") names.push(identifier.name);
-  return names;
-}, []);
+  /* reduce templateData to an array of class names */
+  const classNames = templateData.reduce((names, identifier) => {
+    if (identifier.kind === "class" && !names.includes(identifier.name)) names.push(identifier.name);
+    return names;
+  }, []);
 
-/* create a documentation file for each class */
-for (const className of classNames) {
-  // Workaround an issue with the inputFile picking up wrong classes
-  if (["Scanner", "Walker", "Parser", "Filter"].includes(className)) continue;
-  const template = `{{#class name="${className}"}}{{>docs}}{{/class}}`;
-  console.log(`rendering ${className}, template: ${template}`);
-  const output = jsdoc2md.renderSync({
-    data: templateData,
-    template: template
-  });
-  fs.writeFileSync(path.resolve(outputDir, `${className}.md`), output);
+  console.log(classNames)
+
+  /* create a documentation file for each class */
+  for (const className of classNames) {
+    // Workaround an issue with the inputFile picking up wrong classes
+    if (["Scanner", "Walker", "Parser", "Filter"].includes(className)) continue;
+    const template = `{{#class name="${className}"}}{{>docs}}{{/class}}`;
+    console.log(`rendering ${className}, template: ${template}`);
+    const output = jsdoc2md.renderSync({
+      data: templateData,
+      template: template
+    });
+    fs.writeFileSync(path.resolve(outputDir, `${className}.md`), output);
+  }
 }
+
+generate(path.join(__dirname, "../mods/**/dist/client/*.js"))
+generate(path.join(__dirname, "../mods/voice/dist/*.js"))
