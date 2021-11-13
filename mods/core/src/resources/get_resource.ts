a6b83ab95c3dae749b947b582c2524def14e884d
Pedro Sanders <sanderspedro@gmail.com>
2021-11-13 16:51:42 -0500

    Added host assertion

--- mods/core/src/resources/get_resource.ts
+++ mods/core/src/resources/get_resource.ts
@@ -4,12 +4,10 @@ import {GetResourceRequest} from "./types";
 export default async function getResource(
   request: GetResourceRequest
 ): Promise<unknown> {
-  console.log("DBG001");
   await routr.connect();
   const jsonObj = await routr
     .resourceType(`${request.kind.toLowerCase()}s`)
     .get(request.ref);
-  console.log("XXXXXX->" + jsonObj);
   // Return only if exist and is the owner of the resource
   return jsonObj && jsonObj.metadata.accessKeyId === request.accessKeyId
     ? jsonObj
---
+++ mods/providers/src/service/assertions.ts
@@ -0,0 +1,10 @@
+import {FonosterInvalidArgument} from "@fonoster/errors";
+import isValidHost from "is-valid-host";
+
+export const assertIsValidHost = (host: string) => {
+  if (!isValidHost(host)) {
+    throw new FonosterInvalidArgument(
+      `invalid host: ${host}`
+    );
+  }
+};
