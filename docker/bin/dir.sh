5ae234ee516935a5a1a2f23f713ebef1b2ed7f03
Efraa <efra23peralta@gmail.com>
2022-03-01 14:54:41 -0400

    fix(docker): fix indentation

--- docker/bin/dir.sh
+++ docker/bin/dir.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 function dir() {
-    [[ -z "$*" ]] && error "No directories provided"
+  [[ -z "$*" ]] && error "No directories provided"
 
-    for directory in "$@"; do
-        if [ ! -d "$directory" ]; then
-            mkdir -p "$directory"
-        else
-            warning "The directory '$directory' already exists"
-        fi
-    done
+  for directory in "$@"; do
+    if [ ! -d "$directory" ]; then
+      mkdir -p "$directory"
+    else
+      warning "The directory '$directory' already exists"
+    fi
+  done
 }
