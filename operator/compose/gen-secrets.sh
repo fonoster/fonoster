ad0e3618a244fd7b4b7a0ea6c3ab6fb8d4a2f236
Efraa <efra23peralta@gmail.com>
2022-03-02 09:33:33 -0400

    fix(operator): format bash scripts

--- operator/compose/gen-secrets.sh
+++ operator/compose/gen-secrets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function generateSecrets() {
-    openssl rand -hex 16
+  openssl rand -hex 16
 }
 
 MS_ARI_USERNAME=$(grep MS_ARI_USERNAME .env | cut -d '=' -f2)
@@ -13,10 +13,10 @@ DS_SECRET=$(generateSecrets)
 MS_ARI_AUTHORIZATION=$(printf ${MS_ARI_USERNAME}:${MS_ARI_SECRET} | base64)
 
 sed -i.bak \
-    -e "s#MS_ARI_SECRET=.*#MS_ARI_SECRET=${MS_ARI_SECRET}#g" \
-    -e "s#MS_ARI_AUTHORIZATION=.*#MS_ARI_AUTHORIZATION=${MS_ARI_AUTHORIZATION}#g" \
-    -e "s#FS_SECRET=.*#FS_SECRET=${FS_SECRET}#g" \
-    -e "s#SIPPROXY_SECRET=.*#SIPPROXY_SECRET=${SIPPROXY_SECRET}#g" \
-    -e "s#SIPPROXY_API_SECRET=.*#SIPPROXY_API_SECRET=${SIPPROXY_API_SECRET}#g" \
-    -e "s#DS_SECRET=.*#DS_SECRET=${DS_SECRET}#g" \
-    "$(dirname "$0")/.env"
+  -e "s#MS_ARI_SECRET=.*#MS_ARI_SECRET=${MS_ARI_SECRET}#g" \
+  -e "s#MS_ARI_AUTHORIZATION=.*#MS_ARI_AUTHORIZATION=${MS_ARI_AUTHORIZATION}#g" \
+  -e "s#FS_SECRET=.*#FS_SECRET=${FS_SECRET}#g" \
+  -e "s#SIPPROXY_SECRET=.*#SIPPROXY_SECRET=${SIPPROXY_SECRET}#g" \
+  -e "s#SIPPROXY_API_SECRET=.*#SIPPROXY_API_SECRET=${SIPPROXY_API_SECRET}#g" \
+  -e "s#DS_SECRET=.*#DS_SECRET=${DS_SECRET}#g" \
+  "$(dirname "$0")/.env"
