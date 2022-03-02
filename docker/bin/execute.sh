378010a44027f513a62d2f5ea5aaed67977c13da
Efraa <efra23peralta@gmail.com>
2022-03-02 16:02:23 -0400

    feat(docker): hide outputs from external commands

--- docker/bin/execute.sh
+++ docker/bin/execute.sh
@@ -6,8 +6,10 @@ function execute() {
   for command in "$@"; do
     info "Executing command: $command ..."
 
-    if ! $command; then
-      error "Failed to execute command: '$command'"
+    if ! $command >/dev/null 2>&1; then
+      error "Failed to execute command: '$command' ⭕ "
+    else
+      info "Successfully executed command: '$command' ✅ "
     fi
   done
 }
