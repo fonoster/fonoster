b6b2e1f48ced7ee7a00316c4590d1f1c35085766
Efraa <efra23peralta@gmail.com>
2022-03-02 23:17:46 -0400

    refactor(docker): update logic of exec.sh

--- docker/bin/execute.sh
+++ docker/bin/execute.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
 
+COMMAND_LOGS="exec.log"
+
+function exec_command() {
+  local COMMAND="$1"
+
+  if [ -n "$VERBOSE" ]; then
+    eval "$COMMAND"
+  else
+    eval "$COMMAND" >$COMMAND_LOGS 2>&1
+  fi
+}
+
 function execute() {
   [[ -z "$*" ]] && error "No commands provided"
 
   for command in "$@"; do
-    info "Executing command: $command ..."
+    info "Executing command: $command... 🏁 "
 
-    if ! $command >/dev/null 2>&1; then
-      error "Failed to execute command: '$command' ⭕ "
+    if ! exec_command "$command"; then
+      warning "Failed to execute command: '$command'"
+
+      if [ -f $COMMAND_LOGS ]; then
+        line
+
+        warning "Command output: 👀 "
+        warning " -------------------------------------------------- "
+        warning "$(cat $COMMAND_LOGS)"
+        warning " -------------------------------------------------- "
+
+        line
+
+        rm -rf $COMMAND_LOGS
+      fi
+
+      error "Unable to continue. Exiting... ⭕ "
     else
       info "Successfully executed command: '$command' ✅ "
+    fi
+
+    if [ -f $COMMAND_LOGS ]; then
+      rm -rf $COMMAND_LOGS
     fi
   done
 }
