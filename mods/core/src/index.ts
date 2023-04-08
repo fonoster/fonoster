bd8de751ed89296fc092f3717cff0e4b3121befe
Pedro Sanders <psanders@fonoster.com>
2023-04-07 21:56:12 -0400

    chore: removes unused code

--- mods/core/src/index.ts
+++ mods/core/src/index.ts
@@ -16,14 +16,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { Kind, Privacy, ResourceBuilder } from "./common/resource_builder";
+import { routr } from "./common/routr";
 import getAccessKeyId from "./common/get_access_key_id";
 import getAccessKeySecret from "./common/get_access_key_secret";
 import getRedisConnection from "./common/redis";
-import { routr } from "./common/routr";
 import ResourceServer from "./resources/resource_server";
 import createResource from "./resources/create_resource";
 import updateResource from "./resources/update_resource";
-import { Kind, Privacy, ResourceBuilder } from "./common/resource_builder";
 
 export {
   Privacy,
