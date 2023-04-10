547b5d81a4a4d745a261bc286ba0e80163a4f74d
Pedro Sanders <psanders@fonoster.com>
2023-04-09 21:23:22 -0400

    fix: adds missing export

--- mods/domains/src/index.ts
+++ mods/domains/src/index.ts
@@ -16,8 +16,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import Domains, { Domain } from "./client/domains";
+import Domains, { Domain, CommonPB } from "./client/domains";
 import DomainsServer, { DomainsService } from "./service/domains";
 
-export { DomainsServer, DomainsService, Domains as default, Domain };
+export { DomainsServer, DomainsService, Domains as default, Domain, CommonPB };
 export * from "./client/types";
