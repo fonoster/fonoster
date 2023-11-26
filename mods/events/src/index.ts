928fd8f8e01be8df45dbc09bcecd2288d39e0410
Pedro Sanders <psanders@fonoster.com>
2023-11-26 11:24:18 -0500

    chore: remove deprecated packages

--- mods/events/src/index.ts
+++
@@ -1,4 +0,0 @@
-import EventsSender from "./events_sender";
-import EventsRecvr from "./events_recvr";
-
-export { EventsRecvr, EventsSender };
--- mods/funcs/etc/example/function/handler.ts
+++
@@ -1,3 +0,0 @@
-// Example function
-export default async (event: any, context: any) =>
-  context.status(200).succeed("Hello world!");
