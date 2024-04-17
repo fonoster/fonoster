ff0eec3c96875e2b5e3eec72608c79af7164f387
Pedro Sanders <psanders@fonoster.com>
2024-04-17 17:11:25 -0400

    chore: wip initial setup

--- mods/apiserver/src/index.ts
+++ mods/apiserver/src/index.ts
@@ -1,12 +1,33 @@
 #!/usr/bin/env node
-
+/*
+ * Copyright (C) 2024 by Fonoster Inc (https://fonoster.com)
+ * http://github.com/fonoster/fonoster
+ *
+ * This file is part of Fonoster
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 function sleep(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function main() {
+  // eslint-disable-next-line no-loops/no-loops, no-constant-condition
   while (true) {
-    console.log('Hello, world!');
+    console.log("Hello, world!");
     await sleep(3600000);
   }
 }
+
+main().catch(console.error);
