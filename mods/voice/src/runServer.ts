7286d802831ec3380ce7e75141c7e06e20d0b3ab
Pedro Sanders <psanders@fonoster.com>
2024-05-25 22:47:23 -0400

    chore: wip the voice module

--- mods/voice/src/runServer.ts
+++ mods/voice/src/runServer.ts
@@ -26,6 +26,7 @@ import { getLogger } from "@fonoster/logger";
 import * as grpc from "@grpc/grpc-js";
 import { HealthImplementation } from "grpc-health-check";
 import { BIND_ADDR, IDENTITY_PUBLIC_KEY } from "./envs";
+import { serviceDefinition } from "./serviceDefinition";
 
 const logger = getLogger({ service: "voice", filePath: __filename });
 
@@ -41,6 +42,28 @@ async function runServer() {
   // });
 
   const server = new grpc.Server();
+
+  server.addService(serviceDefinition, {
+    createSession: (call) => {
+      logger.info("createSession", call.request);
+
+      call.on("data", (request) => {
+        logger.info("received CreateSessionRequest", request.request);
+        call.write({ answerCommand: { sessionId: "123" } });
+        call.write({ hangupCommand: { sessionId: "123" } });
+        call.end();
+      });
+
+      call.on("end", () => {
+        logger.info("client ended stream");
+        call.end();
+      });
+
+      call.on("error", (error) => {
+        logger.error("stream error", error);
+      });
+    }
+  });
 
   // Add the health check service to the server
   healthImpl.addToServer(server);
