7bed2e13577ace1cdbbc4859618f5c2a6ae8311e
Pedro Sanders <psanders@fonoster.com>
2024-07-25 17:30:47 -0400

    fix: fix files casing issue

---
+++ mods/identity/src/apikeys/createApiKey.ts
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2024 by Fonoster Inc (https://fonoster.com)
+ * http://github.com/fonoster/fonoster
+ *
+ * This file is part of Fonoster
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { GrpcErrorMessage, handleError } from "@fonoster/common";
+import { getLogger } from "@fonoster/logger";
+import { ServerInterceptingCall } from "@grpc/grpc-js";
+import { z } from "zod";
+import { ApiRoleEnum } from "./ApiRoleEnum";
+import { Prisma } from "../db";
+import { getAccessKeyIdFromCall } from "../utils";
+import {
+  AccessKeyIdType,
+  generateAccessKeyId
+} from "../utils/generateAccessKeyId";
+import { generateAccessKeySecret } from "../utils/generateAccessKeySecret";
+
+const logger = getLogger({ service: "identity", filePath: __filename });
+
+const CreatApiKeyRequestSchema = z.object({
+  role: z.enum([ApiRoleEnum.WORKSPACE_ADMIN]),
+  expiresAt: z.number().transform((value) => (value === 0 ? null : value))
+});
+
+type CreateApiKeyRequest = z.infer<typeof CreatApiKeyRequestSchema>;
+
+type CreateApiKeyResponse = {
+  ref: string;
+  accessKeyId: string;
+  accessKeySecret: string;
+};
+
+function createApiKey(prisma: Prisma) {
+  return async (
+    call: { request: CreateApiKeyRequest },
+    callback: (error: GrpcErrorMessage, response?: CreateApiKeyResponse) => void
+  ) => {
+    try {
+      const validatedRequest = CreatApiKeyRequestSchema.parse(call.request);
+
+      const accessKeyId = getAccessKeyIdFromCall(
+        call as unknown as ServerInterceptingCall
+      );
+
+      const { role, expiresAt } = validatedRequest;
+
+      logger.info("creating new ApiKey", { accessKeyId, role, expiresAt });
+
+      const workspace = await prisma.workspace.findUnique({
+        where: { accessKeyId }
+      });
+
+      const response = await prisma.apiKey.create({
+        data: {
+          workspaceRef: workspace.ref,
+          role: validatedRequest.role,
+          accessKeyId: generateAccessKeyId(AccessKeyIdType.API_KEY),
+          accessKeySecret: generateAccessKeySecret(),
+          expiresAt: expiresAt ? new Date(expiresAt) : null
+        }
+      });
+
+      callback(null, {
+        ref: response.ref,
+        accessKeyId: response.accessKeyId,
+        accessKeySecret: response.accessKeySecret
+      });
+    } catch (error) {
+      handleError(error, callback);
+    }
+  };
+}
+
+export { createApiKey };
---
+++ mods/identity/src/exchanges/exchangeApiKey.ts
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2024 by Fonoster Inc (https://fonoster.com)
+ * http://github.com/fonoster/fonoster
+ *
+ * This file is part of Fonoster
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { GrpcErrorMessage, handleError } from "@fonoster/common";
+import { getLogger } from "@fonoster/logger";
+import * as grpc from "@grpc/grpc-js";
+import { z } from "zod";
+import { exchangeTokens } from "./exchangeTokens";
+import { IdentityConfig } from "./types";
+import { Prisma } from "../db";
+import { getApiKeyByAccessKeyId } from "../utils/getApiKeyByAccessKeyId";
+
+const logger = getLogger({ service: "identity", filePath: __filename });
+
+const ExchangeApiKeysRequestSchema = z.object({
+  accessKeyId: z.string(),
+  accessKeySecret: z.string()
+});
+
+type ExchangeApiKeysRequest = z.infer<typeof ExchangeApiKeysRequestSchema>;
+
+type ExchangeApiKeysResponse = {
+  idToken: string;
+  accessToken: string;
+  refreshToken: string;
+};
+
+const invalidApiKeyError = {
+  code: grpc.status.PERMISSION_DENIED,
+  message: "Invalid credentials"
+};
+
+function exchangeApiKey(prisma: Prisma, identityConfig: IdentityConfig) {
+  return async (
+    call: { request: ExchangeApiKeysRequest },
+    callback: (
+      error: GrpcErrorMessage,
+      response?: ExchangeApiKeysResponse
+    ) => void
+  ) => {
+    try {
+      const validatedRequest = ExchangeApiKeysRequestSchema.parse(call.request);
+      const { accessKeyId, accessKeySecret } = validatedRequest;
+
+      logger.verbose("call to exchangeApiKey", { accessKeyId });
+
+      const key = await getApiKeyByAccessKeyId(prisma)(accessKeyId);
+
+      if (key?.accessKeySecret !== accessKeySecret?.trim()) {
+        return callback(invalidApiKeyError);
+      }
+
+      return callback(
+        null,
+        await exchangeTokens(prisma, identityConfig)(accessKeyId)
+      );
+    } catch (error) {
+      handleError(error, callback);
+    }
+  };
+}
+
+export { exchangeApiKey };
