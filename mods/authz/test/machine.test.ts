ad25b8135de1f8e6a4d91ea4157551b83d7bb235
Pedro Sanders <psanders@fonoster.com>
2024-12-17 17:12:19 -0500

    chore: wip the authz module

---
+++ mods/authz/test/machine.test.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-invalid-this */
+/*
+ * Copyright (C) 2024 by Fonoster Inc (https://fonoster.com)
+ * http://github.com/fonoster/fonoster
+ *
+ * This file is part of Fonoster
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import * as chai from "chai";
+import { expect } from "chai";
+import chaiAsPromised from "chai-as-promised";
+import { createSandbox } from "sinon";
+import sinonChai from "sinon-chai";
+
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+const sandbox = createSandbox();
+
+describe("@authz", function () {
+  afterEach(function () {
+    return sandbox.restore();
+  });
+
+  it("needs tests", async function () {
+  });
+});
