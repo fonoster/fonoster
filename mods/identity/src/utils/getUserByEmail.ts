45e9de6a47384d6bdeaee448aae4a77c165f4ad5
Pedro Sanders <psanders@fonoster.com>
2024-12-23 11:13:52 -0500

    chore: remove useless conditional from the getUserByEmail function

--- mods/identity/src/utils/getUserByEmail.ts
+++ mods/identity/src/utils/getUserByEmail.ts
@@ -20,17 +20,11 @@ import { Prisma } from "../db";
 
 function getUserByEmail(prisma: Prisma) {
   return async (email: string) => {
-    const user = await prisma.user.findFirst({
+    return await prisma.user.findFirst({
       where: {
         email
       }
     });
-
-    if (!user) {
-      return null;
-    }
-
-    return user;
   };
 }
 
