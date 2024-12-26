a6ec225567e1cd98bd0b11c32e5b338769dfc4cc
obrucheoghene <wilfredcloudspace@gmail.com>
2024-12-26 17:37:57 +0100

    styles: fix arrow styling and spacing of tip container
    Signed-off-by: obrucheoghene <wilfredcloudspace@gmail.com>

--- mods/webui/src/stories/tooltip/Tooltip.styles.ts
+++ mods/webui/src/stories/tooltip/Tooltip.styles.ts
@@ -23,7 +23,7 @@ import { TooltipPlacement } from "./types";
 const arrowSize = "6px";
 const translateX0Y_50 = "translateX(0) translateY(-50%)";
 const translateX_50 = "translateX(-50%)";
-const tooltipDistance = "calc(100% + 10px)";
+const tooltipDistance = "calc(100% + 12px)";
 
 export const StyledTooltipContainer = styled("div")(() => ({
   position: "relative"
@@ -36,9 +36,8 @@ export const StyledTooltipContentList = styled("div")<{
   left: "50%",
   backgroundColor: theme.palette.secondary[700],
   borderRadius: "4px",
-  padding: "5px",
-  paddingLeft: "10px",
-  paddingRight: "10px",
+  padding: "10px",
+  paddingTop: '5px',
   whiteSpace: "nowrap",
   transform: "translateX(-50%)",
   zIndex: 100,
@@ -58,7 +57,7 @@ export const StyledTooltipArrow = styled("span")<{
   height: 0,
   width: 0,
   pointerEvents: "none",
-  borderWidth: arrowSize,
+  borderWidth: '10px',
   ...getTooltipArrowStyles(placement, theme)
 }));
 
@@ -90,7 +89,7 @@ const getTooltipPositionStyles = (placement: TooltipPlacement) => {
   }
 };
 
-const getTooltipArrowStyles = (placement: TooltipPlacement, theme: Theme) => {
+const getTooltipArrowStyles = (placement: TooltipPlacement, theme: Theme): React.CSSProperties => {
   const backgroundColor = theme.palette.secondary[700];
 
   switch (placement) {
@@ -99,29 +98,32 @@ const getTooltipArrowStyles = (placement: TooltipPlacement, theme: Theme) => {
         top: "100%",
         left: "50%",
         transform: translateX_50,
-        borderTopColor: backgroundColor
+        borderTopColor: backgroundColor,
+        borderTopWidth: arrowSize
       };
     case "right":
       return {
         right: `100%`,
         top: "50%",
         transform: translateX0Y_50,
-        borderRightColor: backgroundColor
+        borderRightColor: backgroundColor,
+        borderRightWidth: arrowSize
       };
     case "bottom":
       return {
         bottom: "100%",
         left: "50%",
         transform: translateX_50,
-        borderBottomColor: backgroundColor
+        borderBottomColor: backgroundColor,
+        borderBottomWidth: arrowSize
       };
     case "left":
       return {
-        left: "auto",
-        right: `calc(-2 * ${arrowSize})`,
+        left: `100%`,
         top: "50%",
         transform: translateX0Y_50,
-        borderLeftColor: backgroundColor
+        borderLeftColor: backgroundColor,
+        borderLeftWidth: arrowSize
       };
     default:
       return {};
