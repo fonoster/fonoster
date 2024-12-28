810aff3b45d11a3bb36248fefcca23ca3d77081e
Pedro Sanders <psanders@fonoster.com>
2024-12-28 12:01:50 -0500

    fix: correct reference to VoiceLanguage object

--- mods/apiserver/src/voice/stt/types.ts
+++ mods/apiserver/src/voice/stt/types.ts
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { VoiceLanguage } from "@fonoster/common/src/tts/types";
+import { VoiceLanguage } from "@fonoster/common";
 
 type SpeechResult = {
   speech: string;
