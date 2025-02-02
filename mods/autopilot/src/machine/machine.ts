417aedc4f59fecfdad64471c778bdb8deaf0f3e0
Pedro Sanders <psanders@fonoster.com>
2025-02-01 19:11:17 -0500

    chore: clean up the Autopilot's fsm

--- mods/autopilot/src/machine/machine.ts
+++ mods/autopilot/src/machine/machine.ts
@@ -229,12 +229,6 @@ const machine = setup({
       }
     )
   },
-  on: {
-    ERROR: {
-      target: "systemError",
-      actions: "logError"
-    }
-  },
   after: {
     SESSION_TIMEOUT: {
       target: "hangup",
@@ -242,6 +236,7 @@ const machine = setup({
     }
   }
 }).createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5QDMB2BBAkgOigJzDABcBLVKAYgG0AGAXUVAAcB7WE0l1RkAD0QCMADgHYAzAHYAnADYJYgExCaMmSJkAaEAE9EUsQFZsUiaoUGpAhQsMSAvna1os2EhAA2YCgGUACgFF-AGEACQB9bwAVdAAlSNoGJBBWdk5uJP4EAQMAFiFxHLkJbLEaAxkBCS1dLIEcnPFFZSkpHIklQwcnDBw3Tx8A4PCY-28AVQAZePoeFI4SLh5MyzFsBUrzHJMrAxtqxAkcoxyFHMqt0pbsrpBnXo8vXlgiAEMiMGwX5He8AApMAAiE38YUimAAsv4APJjSIASgod1cDwSszY80WGUQMgMRgEUhokgMYjyMlKAn2WQUJmwEnK23a5jEQhuSL6j2ebw+Xx+-yBILBkJh8MRPWRnioAkSzHRaSWgjqNGwQnkNAU0iEYi1VR0emy2Bk+iOpU1bRkrLFRDwL1QqQWqDIUEiLEwD2oMySczlWIQygUtOyORohX0uLJlKkBlE7WJNEqZJVKgtLncJGeYAd5GdY1gYDwA0CoTC-gAcgDUZ7Zfb5VkNdgDMUVAZTYGDJSxDjjMShLGJNI+wJzY5bmLU+nM06WDm8wWhsWy5Lpckq5jQJkBGVRKdTKozhZLO2o8qtQY44dynSysmcGP3hPs7n835C8NRpNpkuvdWfZUpPkGwIVjWJquQKBGg7GB2ZQ0CYYhSDY17YAA7i88zkAAYiweDeEwhAAMYABaRCQAC2YAsAArkQs5FlEsQfmidqrnwggduIzYKCopw0DQdLlJSuISNgbRBpY1LrKcOSIShaFQJh2G4WAhHEWRlHUc+c4jOMUwVjKTHpGuiBiHU9YqFs6y8SqFSUiI4jWJYBKamSJIGNJqGkBhWE4fhRGkeRVEUE8rzvJ83x5r84LoAAGhEgxFgA6lgkSghC0KwgiSIyR5cleYpyl+WpunLvpNZ5A0Hb1AmwaSZS5iiLsWpklYwg9hILLDpl7mOvJ3lKb5qkBUFXKhbykUxRpCVJSlQrpaKLhZd1uU+Sp-lEIujEYgZLEIKeeK4lsgFtW1pRtrqO0CKswZRsGZJBsIQ7dC4TB4CweFwOw5DTngMRgAAjhRcDURAXAfGQABuLAANYfEiz2ve9jpfT9-2Awg4OvW89oJEVX7MeuuwyNgarNpU8akqdNRWPUxg8TQQjrDI9lSR1Ypw29sAfVASN-QDzw0eEdFxDjK5beuPYNJqhyWGamp05SchGHIvEXUIhrGQIDjDqgLAQHAPB3Bt3qGQgAC0F2E8SphCH+GqHEolIm0YLTO-T1u5Gq9SIfghDZYb37GybQj-pIag2zbJxCBGCiE200ikko9TyIh7J+3jiANv6FSDtSFSJ4SUcx328HBoSdKdCzLhWja+mOs6rqeKnot6kY5JRpIzIkh2EZbMq8GM8ISjwQ2iG3hmtdTo+jc1tkEEcXBKzWzIvGUhIKjGNS0iAcyZ6uRXOALZ5CnLQVVFTz60fYC1JxFzxOL8Wd8+0qcViEtH8inIhBE2lAFFMGfxuARMiIP8eQ6Z00MJHM6F1rDCVMDBOkcCVAPRHE9F67NObcxRs8f+21rBHiOvBQcZwWianlgScQJho7kkULkXeDggA */
   context: ({ input }) => ({
     sessionRef: input.voice.sessionRef,
     voice: input.voice,
@@ -268,12 +263,14 @@ const machine = setup({
   states: {
     greeting: {
       always: {
-        target: "idle"
+        target: "idle",
+        description: "Transition to idle after the initial greeting."
       },
       entry: {
         type: "greetUser"
       }
     },
+
     idle: {
       entry: { type: "cleanSpeech" },
       on: {
@@ -283,7 +280,7 @@ const machine = setup({
         },
         SPEECH_RESULT: {
           target: "listeningToUser",
-          description: "User started speaking before SPEECH_START event",
+          description: "Detected speech before SPEECH_START event.",
           actions: [{ type: "appendSpeech" }]
         }
       },
@@ -295,8 +292,7 @@ const machine = setup({
             guard: and(["idleTimeoutCountExceedsMax", not("isSpeaking")])
           },
           {
-            target: "idleTransition",
-            guard: not("isSpeaking"),
+            target: "transitioningToIdle",
             actions: [
               { type: "increaseIdleTimeoutCount" },
               { type: "announceIdleTimeout" }
@@ -305,7 +301,8 @@ const machine = setup({
         ]
       }
     },
-    idleTransition: {
+
+    transitioningToIdle: {
       // This intermediate state is necessary to ensure the IDLE_TIMEOUT
       // event is properly reset and retriggered when returning to idle.
       // Without it, the timer would not restart correctly.
@@ -313,6 +310,7 @@ const machine = setup({
         target: "idle"
       }
     },
+
     listeningToUser: {
       entry: [
         { type: "interruptPlayback" },
@@ -325,53 +323,63 @@ const machine = setup({
             target: "processingUserRequest",
             guard: "hasSpeechResult",
             actions: [{ type: "setSpeakingDone" }],
-            description: "Process immediately if we have speech"
+            description:
+              "Process the request immediately since we already speech."
           },
           {
             target: "waitingForSpeechTimeout",
             guard: not("hasSpeechResult"),
             actions: [{ type: "setSpeakingDone" }],
-            description: "Wait for more speech if no results yet"
+            description:
+              "Wait for more speech since we don't have any speech yet."
           }
         ],
         SPEECH_RESULT: [
           {
             actions: { type: "appendSpeech" },
             guard: "isSpeaking",
-            description: "Just append the speech result when actively speaking"
+            description:
+              "The user is still speaking. With only want to append the speech."
           }
         ]
       }
     },
+
     waitingForSpeechTimeout: {
       on: {
         SPEECH_START: {
           target: "listeningToUser",
-          description: "User started speaking again"
+          description: "User started speaking again."
         },
         SPEECH_RESULT: {
           target: "processingUserRequest",
           actions: {
             type: "appendSpeech"
           },
-          description: "Append final speech and process the request"
+          description: "Append final speech and process the request."
         }
       },
       after: {
         MAX_SPEECH_WAIT_TIMEOUT: [
           {
             target: "processingUserRequest",
+            description:
+              "Proceed to process the request as we have speech and the user is not speaking.",
             guard: "hasSpeechResult"
           },
           {
-            target: "idle"
+            target: "idle",
+            description:
+              "We have no speech and the user is not speaking. Return to idle."
           }
         ]
       }
     },
+
     hangup: {
       type: "final"
     },
+
     processingUserRequest: {
       on: {
         SPEECH_START: {
@@ -382,15 +390,12 @@ const machine = setup({
       },
       invoke: {
         src: "doProcessUserRequest",
+        description: "Process the user request",
         input: ({ context }) => ({ context }),
         onDone: {
           target: "idle"
         }
       }
-    },
-    systemError: {
-      entry: "announceSystemError",
-      target: "idle"
     }
   }
 });
