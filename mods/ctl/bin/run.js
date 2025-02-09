73a96f2fb143bfe0b24535ded8c4f5f1e97f9d0e
Pedro Sanders <psanders@fonoster.com>
2025-02-08 22:40:40 -0500

    chore: add temporary workaround for punycode deprecation message

--- mods/ctl/bin/run.js
+++ mods/ctl/bin/run.js
@@ -1,4 +1,7 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --no-warnings
+
+// WARNING: We added the `--no-warnings` flag to suppress the warning about the punycode deprecated module.
+// This is a temporary workaround until this get's fixed upstream.
 
 // eslint-disable-next-line unicorn/prefer-top-level-await
 (async () => {
