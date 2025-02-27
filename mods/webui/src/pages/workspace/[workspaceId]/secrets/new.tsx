8eeff488ed58921fb89988eaeae279d6f98c31a9
Angel M. Bencosme <ing.angelebencosme@gmail.com>
2025-02-27 11:32:25 -0400

    adding the secrets form, functionalities and validation and the entire base structure.
    Signed-off-by: Angel M. Bencosme <ing.angelebencosme@gmail.com>

---
+++ mods/webui/src/pages/workspace/[workspaceId]/secrets/new.tsx
@@ -0,0 +1,15 @@
+import SecretForm, { SecretFormData } from '@/pages/workspace/[workspaceId]/secrets/_component/form/SecretForm';
+
+export default function NewSecretPage() {
+    const initialData: SecretFormData = {
+        name: '',
+        secret: '',
+    };
+
+    return (
+        <SecretForm
+            formId="secret-form"
+            initialData={initialData}
+        />
+    );
+}
