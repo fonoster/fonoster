ec9dbfcd72585566775d61faa6e3d76eee964d9c
Pedro Sanders <psanders@fonoster.com>
2025-03-01 10:51:55 -0500

    chore: wip improve linting ops

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,21 @@ export default [
         ecmaVersion: 2020,
         sourceType: 'module',
       },
+      globals: {
+        __filename: 'readonly',
+        __dirname: 'readonly',
+        process: 'readonly',
+        require: 'readonly',
+        module: 'readonly',
+        exports: 'writable',
+        Buffer: 'readonly',
+        console: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+        fetch: 'readonly',
+      },
     },
     plugins: {
       '@typescript-eslint': tseslint,
@@ -26,9 +41,22 @@ export default [
       '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'warn'
-    },
+    }
   },
   {
-    ignores: ['**/dist/**', '**/node_modules/**', '**/*.d.ts']
+    ignores: [
+      '**/dist/**', 
+      '**/node_modules/**', 
+      '**/*.d.ts', 
+      '**/bin/**',
+      ".scripts/**",
+      'site/**', 
+      'mods/webui/**', 
+      'mods/**/*.test.ts', 
+      'mods/**/*.proto', 
+      '**/generated/**', 
+      'mods/sdk/public/fonoster.min.js',
+      'mods/**/*.integration.ts'
+    ]
   }
 ];
