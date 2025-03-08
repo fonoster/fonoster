c1f3f80f5a93c8f13e0d9255417627565db328ba
Angel M. Bencosme <ing.angelbencosme@gmail.com>
2025-03-08 10:01:25 -0400

    corrections in the layout and centralizing the meta viewport tag to avoid warnings
    Signed-off-by: Angel M. Bencosme <ing.angelbencosme@gmail.com>

--- mods/webui/src/pages/_app.tsx
+++ mods/webui/src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { useRouter } from "next/router";
+import Head from "next/head";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
-import { NoAuthLayout } from "@/common/components/layout/noAuth/NoAuthLayout";
-import { LayoutWrapper as LayoutAuth } from "@/common/components/layout/auth/LayoutWrapper";
+import {  Layout as LayoutAuth } from "@/common/components/layout/auth/Layout";
 import { FonosterProvider } from "@/common/sdk/provider/FonosterContext";
 import { WorkspaceProvider } from "@/common/sdk/provider/WorkspaceContext";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import type { AppProps } from "next/app";
 import { fnLight } from "@theme/theme";
+
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
@@ -25,18 +26,22 @@ export default function App({ Component, pageProps }: AppProps) {
     <AppRouterCacheProvider>
       <ThemeProvider theme={fnLight}>
         <CssBaseline />
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, viewport-fit=cover"
+          />
+        </Head>
         <FonosterProvider>
-          {isPageNotAuthentication ? (
-            <NoAuthLayout>
-              <Component {...pageProps} />
-            </NoAuthLayout>
-          ) : (
-            <WorkspaceProvider>
-              <LayoutAuth>
-                <Component {...pageProps} />
-              </LayoutAuth>
-            </WorkspaceProvider>
-          )}
+          {isPageNotAuthentication ?
+            <Component {...pageProps} />
+            : (
+              <WorkspaceProvider>
+                <LayoutAuth>
+                  <Component {...pageProps} />
+                </LayoutAuth>
+              </WorkspaceProvider>
+            )}
         </FonosterProvider>
       </ThemeProvider>
     </AppRouterCacheProvider>
