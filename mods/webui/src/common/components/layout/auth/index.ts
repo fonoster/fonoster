590735069be92649e855af4171a982f1a03d743c
Angel M. Bencosme <ing.angelbencosme@gmail.com>
2025-03-08 10:15:24 -0400

    simplifying the index of the authorized layout.
    Signed-off-by: Angel M. Bencosme <ing.angelbencosme@gmail.com>

--- mods/webui/src/common/components/layout/auth/index.ts
+++ mods/webui/src/common/components/layout/auth/index.ts
@@ -1,3 +1,1 @@
-export { LayoutWrapper } from "@/common/components/layout/auth/LayoutWrapper";
-export { FullLayout } from "@/pages/workspace/_components/layouts/FullLayout";
-export { SimpleLayout } from "@/common/components/layout/auth/SimpleLayout";
+export { LayoutWrapper } from "@/common/components/layout/auth/Layout";
