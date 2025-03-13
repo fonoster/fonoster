460bd783832e5392dd350c1a93d1df15010299f8
Angel M. Bencosme <ing.angelbencosme@gmail.com>
2025-03-13 19:50:17 -0400

    overall margin for the page container and removing some unnecessary backgroups.
    Signed-off-by: Angel M. Bencosme <ing.angelbencosme@gmail.com>

--- mods/webui/src/common/components/layout/pages/index.tsx
+++ mods/webui/src/common/components/layout/pages/index.tsx
@@ -32,7 +32,7 @@ interface ContentFormProps<T extends object> {
 }
 
 function PageContainer({ children }: PageContainerProps) {
-  return <Box>{children}</Box>;
+  return <Box sx={{ mb: 8 }}>{children}</Box>;
 }
 
 interface HeaderProps {
@@ -114,14 +114,13 @@ function ContentForm<T extends object>({
     <Box
       component="form"
       id={formId}
+
       sx={{
         display: "flex",
         flexDirection: "column",
         gap: 3,
-        p: 3,
         borderColor: "divider",
         borderRadius: 1,
-        bgcolor: "background.paper",
         "& .MuiFormControl-root": {
           width: "100%",
           maxWidth: "500px"
