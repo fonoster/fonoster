0b46d648ccb4cd27c33fc009bbc2791facaafe39
Angel M. Bencosme <ing.angelbencosme@gmail.com>
2025-03-13 19:52:44 -0400

    improvements to the select that did not support the default value with the react hook form register.
    Signed-off-by: Angel M. Bencosme <ing.angelbencosme@gmail.com>

--- mods/webui/src/common/hooksForm/SelectContext.tsx
+++ mods/webui/src/common/hooksForm/SelectContext.tsx
@@ -25,24 +25,50 @@ const SelectContext = ({
   leadingIcon,
   trailingIcon,
   disabled = false,
-  defaultValue = { value: "", label: "" }
+  defaultValue = { value: "", label: "" },
+  id
 }: SelectContextProps) => {
   const {
     register,
+    setValue,
+    watch,
     formState: { errors }
   } = useFormContext();
 
+  const getNestedError = (name: string) => {
+    if (!name.includes('.')) return errors[name];
+
+    const parts = name.split('.');
+    let error = errors as any;
+
+    for (const part of parts) {
+      if (!error || !error[part]) return undefined;
+      error = error[part];
+    }
+
+    return error;
+  };
+
+  const error = getNestedError(name);
+
+  const { ref, onChange, onBlur, name: fieldName } = register(name);
+  const fieldValue = watch(name);
+
   return (
     <Select
-      {...register(name)}
+      id={id}
       label={label}
-      error={!!errors[name]}
-      supportingText={errors[name]?.message?.toString() || ""}
+      error={!!error}
+      supportingText={error?.message?.toString() || ""}
       options={options}
       leadingIcon={leadingIcon}
       trailingIcon={trailingIcon}
       disabled={disabled}
-      defaultValue={defaultValue.value}
+      name={fieldName}
+      ref={ref}
+      onBlur={onBlur}
+      onChange={onChange}
+      value={fieldValue || defaultValue.value}
     />
   );
 };
