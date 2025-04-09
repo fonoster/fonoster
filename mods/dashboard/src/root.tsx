35a465de69ba0a158e492fe90585153f5d07981b
Efrain Peralta <efrain.peralta@fonoster.com>
2025-04-09 19:32:57 -0400

    feat(dashboard): add fonts

--- mods/dashboard/src/root.tsx
+++ mods/dashboard/src/root.tsx
@@ -38,7 +38,11 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+    href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
   },
   { rel: "stylesheet", href: stylesheet }
 ];
