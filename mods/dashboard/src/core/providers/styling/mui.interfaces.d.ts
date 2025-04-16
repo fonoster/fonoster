ad86ccd1c1d171a765280c9468af6dde318b757a
Efrain Peralta <efrain.peralta@fonoster.com>
2025-04-16 13:07:51 -0400

    chore: update MUI theme palette to include base and brand colors

--- mods/dashboard/src/core/providers/styling/mui.interfaces.d.ts
+++ mods/dashboard/src/core/providers/styling/mui.interfaces.d.ts
@@ -1,26 +1,50 @@
 import "@mui/material/styles";
 
 declare module "@mui/material/styles" {
-  // interface PaletteOptions {
-  //   primary: {
-  //     "50": string;
-  //     "100": string;
-  //     "200": string;
-  //     "500": string;
-  //     "700": string;
-  //     "800": string;
-  //     "900": string;
-  //   };
-  // }
-  // interface Palette {
-  //   primary: {
-  //     "50": string;
-  //     "100": string;
-  //     "200": string;
-  //     "500": string;
-  //     "700": string;
-  //     "800": string;
-  //     "900": string;
-  //   };
-  // }
+  interface PaletteOptions {
+    base: {
+      "01": string;
+      "02": string;
+      "03": string;
+      "04": string;
+      "05": string;
+      "06": string;
+      "07": string;
+      "08": string;
+      main: string;
+    }
+    brand: {
+      "01": string;
+      "02": string;
+      "03": string;
+      "04": string;
+      "05": string;
+      "06": string;
+      "07": string;
+      main: string;
+    };
+  }
+  interface Palette {
+    base: {
+      "01": string;
+      "02": string;
+      "03": string;
+      "04": string;
+      "05": string;
+      "06": string;
+      "07": string;
+      "08": string;
+      main: string;
+    }
+    brand: {
+      "01": string;
+      "02": string;
+      "03": string;
+      "04": string;
+      "05": string;
+      "06": string;
+      "07": string;
+      main: string;
+    };
+  }
 }
