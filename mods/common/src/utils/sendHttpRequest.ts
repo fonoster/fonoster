1537da486be277c61bcae41e2f7c90e41908c939
Pedro Sanders <psanders@fonoster.com>
2025-05-05 15:05:53 -0400

    fix: add missing import

--- mods/common/src/utils/sendHttpRequest.ts
+++ mods/common/src/utils/sendHttpRequest.ts
@@ -16,6 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { URLSearchParams } from "url";
 import { getLogger } from "@fonoster/logger";
 import { z } from "zod";
 
