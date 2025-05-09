b53f4e34060758fa6258501436a30791e411d8a0
Efrain Peralta <efrain.peralta@fonoster.com>
2025-05-09 10:38:47 -0400

    chore: add Reset Password page and form components

---
+++ mods/dashboard/src/auth/pages/reset-password/reset-password.form.tsx
@@ -0,0 +1,102 @@
+/**
+ * Copyright (C) 2025 by Fonoster Inc (https://fonoster.com)
+ * http://github.com/fonoster/fonoster
+ *
+ * This file is part of Fonoster
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { type UseFormReturn } from "react-hook-form";
+import type { Schema } from "./reset-password.page";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem
+} from "~/core/components/design-system/forms";
+import { Input } from "~/core/components/design-system/ui/input/input";
+import { FormRoot } from "~/core/components/design-system/forms/form-root";
+import { Box, styled } from "@mui/material";
+import { Button } from "~/core/components/design-system/ui/button/button";
+import { Typography } from "~/core/components/design-system/ui/typography/typography";
+import { Link } from "~/core/components/general/link/link";
+
+export interface ResetPasswordFormProps extends React.PropsWithChildren {
+  form: UseFormReturn<Schema>;
+  onSubmit: (data: Schema) => Promise<void>;
+}
+
+export function ResetPasswordForm({ form, onSubmit }: ResetPasswordFormProps) {
+  const { isValid, isSubmitting } = form.formState;
+  const isSubmitDisabled = !isValid || isSubmitting;
+
+  return (
+    <Form {...form}>
+      <FormRoot onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="password"
+                  label="Password"
+                  supportingText="Please enter your new password"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="password"
+                  label="Confirm Password"
+                  supportingText="Please confirm your new password"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <ActionsRoot>
+          <Button type="submit" isFullWidth disabled={isSubmitDisabled}>
+            {isSubmitting ? "Loading..." : "Reset Password"}
+          </Button>
+
+          <Link to="/auth/login">
+            <Typography variant="body-small" color="base.03">
+              Back to Sign In
+            </Typography>
+          </Link>
+        </ActionsRoot>
+      </FormRoot>
+    </Form>
+  );
+}
+
+export const ActionsRoot = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: theme.spacing(2),
+  textAlign: "center",
+  marginTop: theme.spacing(2)
+}));
---
+++ mods/dashboard/src/auth/pages/reset-password/reset-password.page.tsx
@@ -0,0 +1,87 @@
+/**
+ * Copyright (C) 2025 by Fonoster Inc (https://fonoster.com)
+ * http://github.com/fonoster/fonoster
+ *
+ * This file is part of Fonoster
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { ResetPasswordForm } from "./reset-password.form";
+import { useCallback } from "react";
+import type { Route } from "./+types";
+import { Box } from "@mui/material";
+import { Typography } from "~/core/components/design-system/ui/typography/typography";
+import { toast } from "~/core/components/design-system/ui/toaster/toaster";
+
+export function meta(_: Route.MetaArgs) {
+  return [{ title: "Reset Password | Fonoster" }];
+}
+
+export const schema = z.object({
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*?&)"
+    ),
+  confirmPassword: z.string()
+});
+
+export const resolver = zodResolver(schema);
+
+export type Schema = z.infer<typeof schema>;
+
+export default function ResetPasswordPage() {
+  const form = useForm<Schema>({
+    resolver,
+    defaultValues: {
+      password: "",
+      confirmPassword: ""
+    },
+    mode: "onChange"
+  });
+
+  const onSubmit = useCallback(
+    async (data: Schema) => {
+      console.log("Form submitted", data);
+      toast("Ahoy! Your password has been reset successfully");
+    },
+    [form]
+  );
+
+  return (
+    <Box
+      width="100%"
+      maxWidth="440px"
+      gap="40px"
+      display="flex"
+      flexDirection="column"
+    >
+      <Box gap="16px" display="flex" flexDirection="column" textAlign="center">
+        <Typography variant="heading-large" color="base.03">
+          Reset your password
+        </Typography>
+        <Typography variant="body-small" color="base.03">
+          Please reset your password using 8+ characters with upper, lower,
+          number, and symbol.
+        </Typography>
+      </Box>
+      <ResetPasswordForm {...{ form, onSubmit }} />
+    </Box>
+  );
+}
