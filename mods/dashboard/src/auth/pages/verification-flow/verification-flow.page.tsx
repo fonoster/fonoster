f4cf34aa15dab2c7e9d86953d8acbba9ffcbe8db
Efrain Peralta <efrain.peralta@fonoster.com>
2025-05-09 10:39:45 -0400

    chore: add Verification Flow page component

---
+++ mods/dashboard/src/auth/pages/verification-flow/verification-flow.page.tsx
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2025 by Fonoster Inc (https://fonoster.com)
+ * http://github.com/fonoster/fonoster
+ *
+ * This file is part of Fonoster
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import type { Route } from "./+types";
+
+export function meta(_: Route.MetaArgs) {
+  return [{ title: "Verify Account | Fonoster" }];
+}
+
+export default function VerificationFlow() {
+  return (
+    <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+      <h1>Fonoster Verification Flow</h1>
+    </div>
+  );
+}
