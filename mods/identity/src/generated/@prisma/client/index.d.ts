bfb9b5811a4eddf5d6146a9aa54f6146fff3e9d6
Pedro Sanders <psanders@fonoster.com>
2025-05-16 14:01:54 -0400

    fix: include updated artifacts

--- mods/identity/src/generated/@prisma/client/index.d.ts
+++ mods/identity/src/generated/@prisma/client/index.d.ts
@@ -314,8 +314,8 @@ export namespace Prisma {
   export import Exact = $Public.Exact
 
   /**
-   * Prisma Client JS version: 6.7.0
-   * Query Engine version: 3cff47a7f5d65c3ea74883f1d736e41d68ce91ed
+   * Prisma Client JS version: 6.8.2
+   * Query Engine version: 2060c79ba17c6bb9f5823312b6f6b7f4a845738e
    */
   export type PrismaVersion = {
     client: string
