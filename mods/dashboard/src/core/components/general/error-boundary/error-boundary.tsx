e535fa8363dcd64b6af6b3aa4f56ebbd5e4f5283
Efrain Peralta <efrain.peralta@fonoster.com>
2025-05-16 16:07:03 -0400

    refactor: enhance ErrorLayout component with structured error messaging and navigation

--- mods/dashboard/src/core/components/general/error-boundary/error-boundary.tsx
+++ mods/dashboard/src/core/components/general/error-boundary/error-boundary.tsx
@@ -1,3 +1,7 @@
+import { styled } from "@mui/material";
+import { Button } from "../../design-system/ui/button/button";
+import { useNavigate } from "react-router";
+
 /*
  * Copyright (C) 2025 by Fonoster Inc (https://fonoster.com)
  * http://github.com/fonoster/fonoster
@@ -17,15 +21,52 @@
  * limitations under the License.
  */
 export const ErrorLayout = ({ errorCode }: { errorCode: number }) => {
+  const navigate = useNavigate();
+
   const errorMessage =
-    errorCode === 404 ? "Page not found" : "Something went wrong";
+    errorCode === 404
+      ? "Oops! Page not found"
+      : "Oops! You've encountered an error. Please try again later.";
 
   return (
-    <div className="error-layout">
-      <code>
-        <p>{errorMessage}</p>
-        <p>Please try again later.</p>
-      </code>
-    </div>
+    <ErrorRoot>
+      <ErrorMessage>
+        <ErrorCode>{errorCode}</ErrorCode>
+        <ErrorDescription>{errorMessage}</ErrorDescription>
+      </ErrorMessage>
+      <Button onClick={() => navigate("/")}>Go to Home</Button>
+    </ErrorRoot>
   );
 };
+
+export const ErrorRoot = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100vh",
+  backgroundColor: theme.palette.background.default,
+  color: theme.palette.text.primary,
+  gap: theme.spacing(2)
+}));
+
+export const ErrorMessage = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  textAlign: "center",
+  fontSize: theme.typography.h4.fontSize,
+  color: theme.palette.error.main
+}));
+
+export const ErrorCode = styled("div")(({ theme }) => ({
+  fontSize: theme.typography.h1.fontSize,
+  fontWeight: "bold",
+  color: theme.palette.base["03"]
+}));
+
+export const ErrorDescription = styled("div")(({ theme }) => ({
+  fontSize: theme.typography.body1.fontSize,
+  color: theme.palette.base["04"]
+}));
