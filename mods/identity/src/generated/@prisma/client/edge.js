89b40bee6c8f84f701758c7aa8a36f3546f2b519
Pedro Sanders <psanders@fonoster.com>
2025-06-11 10:58:56 -0400

    chore: add the dashboard to envoy routes

--- mods/identity/src/generated/@prisma/client/edge.js
+++ mods/identity/src/generated/@prisma/client/edge.js
@@ -234,7 +234,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- mods/identity/src/generated/@prisma/client/index.js
+++ mods/identity/src/generated/@prisma/client/index.js
@@ -235,7 +235,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
