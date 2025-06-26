0cf268048d974bdc5b37b15af735923a8c3765a7
Efrain Peralta <efrain.peralta@fonoster.com>
2025-06-26 18:34:16 -0400

    fix: enhance logging for formatted application data submission

--- mods/dashboard/src/applications/services/format-application-data.ts
+++ mods/dashboard/src/applications/services/format-application-data.ts
@@ -49,7 +49,7 @@ export function formatApplicationData({ intelligence, ...data }: Schema) {
     }
   }
 
-  Logger.debug("Formatted application data for submission", application);
+  Logger.debug("[Applications]: Formatted application data for submission", application);
 
   return application;
 }
