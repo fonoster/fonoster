e886082a055735b77355c39ce28bd37fc0c6816d
Efrain Peralta <efrain.peralta@fonoster.com>
2025-08-22 10:49:56 -0400

    chore: remove unused React import from password strength bar component

--- mods/dashboard/src/core/components/design-system/ui/password-strength-bar/password-strength-bar.tsx
+++ mods/dashboard/src/core/components/design-system/ui/password-strength-bar/password-strength-bar.tsx
@@ -16,8 +16,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-import React from "react";
 import { Box, styled } from "@mui/material";
 import { Typography } from "../typography/typography";
 import {
